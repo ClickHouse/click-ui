870089074c0d2f0638d5509ebeb49d486e996c3b
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2023-05-24 13:26:45 +0100

    Remove hover and active button classes

--- app/src/components/ButtonGroup/index.tsx
+++ app/src/components/ButtonGroup/index.tsx
@@ -1,24 +1,39 @@
-import { assert } from "console";
 import React from "react";
 import styled from "styled-components";
 
 export interface ButtonGroupProps {
   labels: string[];
+  activeIndex?: number;
 }
 
-const ButtonGroup = ({ labels }: ButtonGroupProps) => {
-  assert(labels.length > 1);
+const ButtonGroup = ({ labels, activeIndex }: ButtonGroupProps) => {
   const btns = labels.map((label, index) => {
     if (index === 0) {
-      return <ButtonLeft>{label}</ButtonLeft>;
+      return (
+        <ButtonLeft key={index} active={index === activeIndex}>
+          {label}
+        </ButtonLeft>
+      );
     } else if (index === labels.length - 1) {
-      return <ButtonRight>{label}</ButtonRight>;
+      return (
+        <ButtonRight key={index} active={index === activeIndex}>
+          {label}
+        </ButtonRight>
+      );
     } else {
-      return <ButtonCenter>{label}</ButtonCenter>;
+      return (
+        <ButtonCenter key={index} active={index === activeIndex}>
+          {label}
+        </ButtonCenter>
+      );
     }
   });
   return <ButtonGroupWrapper>{btns}</ButtonGroupWrapper>;
 };
+
+interface ButtonProps {
+  active: boolean;
+}
 
 const ButtonGroupWrapper = styled.div`
   box-sizing: border-box;
@@ -27,12 +42,12 @@ const ButtonGroupWrapper = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0px 1px;
-  background: #282828;
   border: 1px solid rgba(65, 65, 65, 0.3);
   border-radius: 4px;
+  background: var(--click/button/group/color/stroke/panel);
 `;
 
-const ButtonCommon = styled.button`
+const ButtonCommon = styled.button<ButtonProps>`
   box-sizing: border-box;
   display: flex;
   flex-direction: row;
@@ -41,16 +56,14 @@ const ButtonCommon = styled.button`
   padding: 5.5px 16px;
   gap: 10px;
 
-  background: #282828;
-`;
+  background: ${({ active }: ButtonProps) =>
+    active
+      ? "var(--click-button-group-color-background-active)"
+      : "var(--click-button-group-color-background-default)"};
 
-const ButtonHoverCommon = styled(ButtonCommon)`
-  background: #2f2f2f;
-`;
-
-const ButtonActiveCommon = styled(ButtonCommon)`
-  background: #383838;
-  border: 1px solid rgba(65, 65, 65, 0.3);
+  &:hover {
+    background: var(--click-button-group-color-background-hover);
+  }
 `;
 
 const ButtonLeft = styled(ButtonCommon)`
@@ -62,30 +75,6 @@ const ButtonCenter = styled(ButtonCommon)`
 `;
 
 const ButtonRight = styled(ButtonCommon)`
-  border-radius: 0px 4px 4px 0px;
-`;
-
-const ButtonLeftHover = styled(ButtonHoverCommon)`
-  border-radius: 4px 0px 0px 4px;
-`;
-
-const ButtonCenterHover = styled(ButtonHoverCommon)`
-  /* State=Center - Hover */
-`;
-
-const ButtonRightHover = styled(ButtonHoverCommon)`
-  border-radius: 0px 4px 4px 0px;
-`;
-
-const ButtonLeftActive = styled(ButtonActiveCommon)`
-  border-radius: 4px 0px 0px 4px;
-`;
-
-const ButtonCenterActive = styled(ButtonActiveCommon)`
-  /* State=Center - Active */
-`;
-
-const ButtonRightActive = styled(ButtonActiveCommon)`
   border-radius: 0px 4px 4px 0px;
 `;
 
