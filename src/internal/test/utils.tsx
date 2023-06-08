6cc2894b143674a5abb5f5cf35fff19db7c7e696
Sergio de Cristofaro <sdecri@gmail.com>
2023-06-08 10:33:59 +0200

    remove unused file

--- src/internal/test/utils.tsx
+++
@@ -1,6 +0,0 @@
-import { ThemeProvider } from "styled-components";
-import { themes } from "@/theme";
-
-export const render = (...props: unknown[]) => (
-  <ThemeProvider theme={themes.dark} {...props} />
-);
