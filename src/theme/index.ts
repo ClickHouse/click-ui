174c7baffa753ffd27125bae21cf5109cfcd3248
Sergio de Cristofaro <sdecri@gmail.com>
2023-06-21 12:09:03 +0200

    update theme types

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,14 @@
-import { merge } from "lodash";
+import { Theme } from "../styles/types";
+import merge from "lodash/merge";
 import * as classicTheme from "../styles/variables.classic.json";
 import * as darkTheme from "../styles/variables.dark.json";
 import * as lightTheme from "../styles/variables.light.json";
 import * as theme from "../styles/variables.json";
 import { DefaultTheme, ThemeProvider } from "styled-components";
+
+declare module "styled-components" {
+  export type DefaultTheme = Theme;
+}
 
 const themes: Record<string, DefaultTheme> = {
   dark: merge({}, theme, darkTheme),
