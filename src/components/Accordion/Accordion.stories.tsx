1dfd7a472f4f53362b6a9b6192805dff4b18d318
Sergio de Cristofaro <sdecri@gmail.com>
2023-06-22 09:44:31 +0200

    add pseudo states

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -29,3 +29,29 @@ export const Small = {
     children: <p>I'm a child</p>,
   },
 };
+
+export const Hover = {
+  args: {
+    title: "Accordion title",
+    size: "large",
+    children: <p>I'm a child</p>,
+  },
+  parameters: {
+    pseudo: {
+      hover: true,
+    },
+  },
+};
+
+export const Active = {
+  args: {
+    title: "Accordion title",
+    size: "large",
+    children: <p>I'm a child</p>,
+  },
+  parameters: {
+    pseudo: {
+      active: true,
+    },
+  },
+};
