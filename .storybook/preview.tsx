ade7a1b407c4e57d19a6f11462cc139ac628e3b1
Sergio de Cristofaro <sdecri@gmail.com>
2023-06-22 12:34:50 +0200

    set dark mode as default

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -26,7 +26,7 @@ export const globalTypes = {
   theme: {
     name: "Theme",
     description: "Global theme for components",
-    defaultValue: "light",
+    defaultValue: "dark",
     toolbar: {
       // The icon for the toolbar item
       icon: "circlehollow",
