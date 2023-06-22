10cd103346e84b09ef48df84e78b15b535054ea5
Sergio de Cristofaro <sdecri@gmail.com>
2023-06-22 12:23:06 +0200

    remove unused code

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,17 +1,6 @@
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { merge } from "lodash";
-import * as classicTheme from "../styles/variables.classic.json";
-import * as darkTheme from "../styles/variables.dark.json";
-import * as lightTheme from "../styles/variables.light.json";
-import * as theme from "../styles/variables.json";
-import { Theme } from "@/styles/types";
-import { ThemeName } from ".";
+import { ThemeName, themes } from ".";
 
-const themes: Record<ThemeName, Theme> = {
-  dark: merge({}, theme, darkTheme),
-  light: merge({}, theme, lightTheme),
-  classic: merge({}, theme, classicTheme),
-};
 const ThemeProvider = ({
   theme: name,
   children,
