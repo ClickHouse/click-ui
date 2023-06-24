594f436209fbc00cfb0e37167038b841499206f6
Sergio de Cristofaro <sdecri@gmail.com>
2023-06-24 09:12:55 +0200

    add global styles to theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,5 +1,16 @@
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import {
+  ThemeProvider as StyledThemeProvider,
+  createGlobalStyle,
+} from "styled-components";
 import { ThemeName, themes } from ".";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    color: ${props => props.theme.click.global.color.text.default};
+    background-color: ${props =>
+      props.theme.click.global.color.background.default}
+  }
+`;
 
 const ThemeProvider = ({
   theme: name,
@@ -8,7 +19,10 @@ const ThemeProvider = ({
   theme: ThemeName;
   children: React.ReactNode;
 }) => (
-  <StyledThemeProvider theme={themes[name]}>{children}</StyledThemeProvider>
+  <StyledThemeProvider theme={themes[name]}>
+    <GlobalStyle />
+    {children}
+  </StyledThemeProvider>
 );
 
 export { ThemeProvider };
