e60f95d9470bcc5474e50fbd3017db3e9863b727
Vineeth Asok Kumar <me@vineethasok.com>
2023-07-14 17:03:39 +0200

    Add Separtor (#49)
    * Add Separtor
    
    * Fix Separator styling
    
    * Fix incorrect orientation assignment
    
    * Remove unwanted import
    
    * Optimize css

---
+++ src/components/Separator/Separator.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import * as RadixSeparator from "@radix-ui/react-separator";
+interface Props extends RadixSeparator.SeparatorProps {
+  size: "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+}
+const CUISeparator = styled(RadixSeparator.Root)<Props>`
+  &[data-orientation="horizontal"] {
+    height: 0.0625rem;
+    width: 100%;
+  }
+  &[data-orientation="vertical"] {
+    height: 100%;
+    width: 0.0625rem;
+  }
+  ${({ theme, size, orientation }) => `
+    background: ${theme.click.separator.color.stroke.default};
+    margin: ${
+      orientation === "horizontal"
+        ? `${theme.click.separator.horizontal.space.y[size]} ${theme.click.separator.horizontal.space.x.all}`
+        : `${theme.click.separator.vertical.space.y.all} ${theme.click.separator.vertical.space.x[size]}`
+    }
+  `}
+`;
+
+const Separator = ({ orientation = "horizontal", ...props }: Props) => (
+  <CUISeparator orientation={orientation} {...props} />
+);
+
+export default Separator;
