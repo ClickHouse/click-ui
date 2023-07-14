94ce9422326ff6f383b404aaae00cecae55d1138
Vineeth Asok Kumar <me@vineethasok.com>
2023-07-14 16:44:04 +0200

    Add Select element (#44)
    * Add Select component
    
    * Add installation changes
    
    * Remove yarn
    
    * Add Select styling changes
    
    * Add Select styling changes
    
    * Fix label text
    
    * Correct svg components
    
    * Order icon type alphabetically
    
    * Fix select styling
    
    * Optimize Select code
    
    * Update package.json lock
    
    * Fix icon changes
    
    * Optimize code
    
    * Fix Icon Size
    
    * Fix Select styling
    
    * Fix select token changes
    
    * Fix select styling

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -8,6 +8,7 @@ import FilterIcon from "../icons/FilterIcon";
 import HistoryIcon from "../icons/HistoryIcon";
 import InsertRowIcon from "../icons/InsertRowIcon";
 import SortAltIcon from "../icons/SortAltIcon";
+import SortIcon from "../icons/SortIcon";
 import { IconProps } from "./types";
 import { ChevronRight } from "../icons/ChevronRight";
 import { ChevronDown } from "../icons/ChevronDown";
@@ -17,21 +18,24 @@ import CheckIcon from "@/components/icons/CheckIcon";
 import InformationIcon from "@/components/icons/InformationIcon";
 import WarningIcon from "@/components/icons/WarningIcon";
 import CrossIcon from "@/components/icons/CrossIcon";
+import ChevronUp from "../icons/ChevronUp";
 
 const ICONS_MAP = {
   "arrow-down": ArrowDown,
   chat: ChatIcon,
   check: CheckIcon,
-  "chevron-right": ChevronRight,
   "chevron-down": ChevronDown,
+  "chevron-right": ChevronRight,
+  "chevron-up": ChevronUp,
   cross: CrossIcon,
   database: DatabaseIcon,
   filter: FilterIcon,
   history: HistoryIcon,
-  information: InformationIcon,
   "info-in-circle": InfoInCircleIcon,
-  insertRow: InsertRowIcon,
-  sort: SortAltIcon,
+  information: InformationIcon,
+  "insert-row": InsertRowIcon,
+  "sort-alt": SortAltIcon,
+  sort: SortIcon,
   user: UserIcon,
   users: UsersIcon,
   warning: WarningIcon,
@@ -74,30 +78,30 @@ const SvgWrapper = styled.div<Partial<IconProps>>`
   align-items: center;
 
   & path[stroke] {
-    stroke: ${props => props.color || "currentColor"};
+    stroke: ${(props) => props.color || "currentColor"};
   }
 
   & path[fill] {
-    fill: ${props => props.color || "currentColor"};
+    fill: ${(props) => props.color || "currentColor"};
   }
 
   & svg {
-    width: ${props =>
+    width: ${(props) =>
       props.width ||
       props.theme.click.image[props.size || "medium"].size.width ||
       "24px"};
-    height: ${props =>
+    height: ${(props) =>
       props.height ||
       props.theme.click.image[props.size || "medium"].size.height ||
       "24px"};
   }
 
   & svg[stroke] {
-    stroke: ${props => props.color || "currentColor"};
+    stroke: ${(props) => props.color || "currentColor"};
   }
 
   & svg[fill]:not([fill="none"]) {
-    fill: ${props => props.color || "currentColor"};
+    fill: ${(props) => props.color || "currentColor"};
   }
 `;
 
