565dac27de68b00b3d8b0c7f0e90fb24deaf087e
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-07-14 15:00:16 +0200

    Fix Checkbox styling

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -15,19 +15,17 @@ export const Checkbox = ({
   onChange,
   ...delegated
 }: CheckboxProps) => (
-  <Wrapper isDisabled={isDisabled} isChecked={isChecked}>
+  <Wrapper>
     <CheckInput
       disabled={isDisabled}
       checked={isChecked}
       onCheckedChange={onChange}
-      data-testid="checkbox"
+      data-testid='checkbox'
       {...delegated}
     >
-      <RadixCheckbox.Indicator>
-        <CheckIconWrapper>
-          <Icon name="check" width={"12px"} height={"12px"} color="white" />
-        </CheckIconWrapper>
-      </RadixCheckbox.Indicator>
+      <CheckIconWrapper>
+        <Icon name='check' size='small' />
+      </CheckIconWrapper>
     </CheckInput>
     <Label isDisabled={isDisabled} isChecked={isChecked}>
       {label}
@@ -36,46 +34,51 @@ export const Checkbox = ({
 );
 
 const Wrapper = styled.div<Partial<CheckboxProps>>`
-  padding: ${props => props.theme.click.checkbox.space.all};
+  padding: ${(props) => props.theme.click.checkbox.space.all};
 
   display: flex;
   align-items: center;
-  gap: ${props => props.theme.click.checkbox.space.gap};
+  gap: ${(props) => props.theme.click.checkbox.space.gap};
 `;
 
 const CheckInput = styled(RadixCheckbox.Root)`
-  border: 1px solid
-    ${props =>
-      props.disabled
-        ? props.theme.click.checkbox.color.stroke.default
-        : props.theme.click.checkbox.color.stroke.disabled};
-
-  border-radius: ${props => props.theme.click.checkbox.radii.all};
-  width: ${props => props.theme.click.checkbox.size.all};
-  height: ${props => props.theme.click.checkbox.size.all};
-  background-color: ${props =>
-    props.disabled
-      ? props.checked
-        ? props.theme.click.checkbox.color.background["disabled-checked"]
-        : props.theme.click.checkbox.color.background["disabled-unchecked"]
-      : props.checked
-      ? props.theme.click.checkbox.color.background.default
-      : "revert"};
   display: flex;
   align-items: center;
   justify-content: center;
+  ${({ theme }) => `
+    border-radius: ${theme.click.checkbox.radii.all};
+    width: ${theme.click.checkbox.size.all};
+    height: ${theme.click.checkbox.size.all};
+    background: ${theme.click.checkbox.color.background.default};
+    border: 1px solid ${theme.click.checkbox.color.stroke.default};
+
+    &[data-state="checked"] {
+      border-color: ${theme.click.checkbox.color.stroke.active};
+      background: ${theme.click.checkbox.color.background.active};
+    }
+    &[data-disabled] {
+      background: ${theme.click.checkbox.color.background["disabled-unchecked"]};
+      border-color: ${theme.click.checkbox.color.stroke.disabled};
+      &[data-state="checked"] {
+        background: ${theme.click.checkbox.color.background["disabled-checked"]};
+        border-color: ${theme.click.checkbox.color.stroke.disabled};
+      }
+    }
+  `};
 `;
 
 const Label = styled.label<Partial<CheckboxProps>>`
-  color: ${props =>
+  color: ${(props) =>
     props.isDisabled
       ? props.theme.click.checkbox.color.label.disabled
       : props.theme.click.checkbox.color.label.default};
 `;
 
-const CheckIconWrapper = styled.div<Partial<CheckboxProps>>`
-  color: ${props =>
-    props.isDisabled
-      ? props.theme.click.checkbox.color.check.disabled
-      : props.theme.click.checkbox.color.check.default};
+const CheckIconWrapper = styled(RadixCheckbox.Indicator)`
+  ${({ theme }) => `
+    color: ${theme.click.checkbox.color.check.default};
+    &[data-disabled] {
+      color: ${theme.click.checkbox.color.check.disabled};
+    }
+  `}
 `;
