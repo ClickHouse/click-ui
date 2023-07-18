7f67ef60399e025815286a18563125ff58f557be
Gareth Jones <github@garethdjones.com>
2023-07-17 21:16:51 -0400

    Fixing type issue.

--- src/styles/types.ts
+++ src/styles/types.ts
@@ -1622,10 +1622,10 @@
         "base": string
       },
       "line-height": {
-        "1": "number",
-        "2": "number",
-        "3": "number",
-        "4": "number"
+        "1": number,
+        "2": number,
+        "3": number,
+        "4": number
       }
     },
     "styles": {
