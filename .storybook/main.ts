49bf842f3d0f9605e9d8f4cbed37241d6f041f9b
Vineeth Asok Kumar <me@vineethasok.com>
2023-07-21 14:40:53 +0200

    Add logo changes (#60)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,5 +14,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  staticDirs: ["../public"],
 };
 export default config;
---
+++ .storybook/manager.ts
@@ -0,0 +1,8 @@
+// .storybook/manager.js
+
+import { addons } from "@storybook/manager-api";
+import theme from "./theme";
+
+addons.setConfig({
+  theme,
+});
