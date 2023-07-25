e50b9c5081e1b2aeaeece218ddd3f7c58c9fee58
Gareth Jones <github@garethdjones.com>
2023-07-24 20:53:34 -0400

    Reordering badge state options

--- src/components/Badge/Badge.stories.ts
+++ src/components/Badge/Badge.stories.ts
@@ -9,6 +9,11 @@ export default {
       options: ["sm", "md"],
       control: { type: "radio" },
     },
+    state: {
+      options: ["default","success","neutral", "info", "warning", "danger", "disabled"],
+      control: { type: "radio" },
+    }
+    
   }
 };
 
