b54b9c6036e5b6f8e2c350ac14a75580fcbcd8cb
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-07-31 16:32:08 +0200

    Add ContextMenu Component

---
+++ src/components/ContextMenu/ContextMenu.stories.tsx
@@ -0,0 +1,50 @@
+import { ContextMenuProps } from "@radix-ui/react-context-menu";
+import { ContextMenu } from "./ContextMenu";
+
+interface Props extends ContextMenuProps {
+  disabled?: boolean;
+  showArrow?: boolean;
+}
+const ContextMenuExample = ({ showArrow, disabled, ...props }: Props) => {
+  return (
+    <ContextMenu {...props}>
+      <ContextMenu.Trigger disabled={disabled}>ContextMenu Trigger</ContextMenu.Trigger>
+      <ContextMenu.Content showArrow={showArrow}>
+        <ContextMenu.Group>
+          <ContextMenu.Item>Content0</ContextMenu.Item>
+        </ContextMenu.Group>
+        <ContextMenu.Item>Content1 long text content</ContextMenu.Item>
+        <ContextMenu.Sub>
+          <ContextMenu.SubTrigger>Hover over</ContextMenu.SubTrigger>
+          <ContextMenu.Content sub>
+            <ContextMenu.Item>SubContent0</ContextMenu.Item>
+            <ContextMenu.Item>SubContent1</ContextMenu.Item>
+            <ContextMenu.Item>SubContent0</ContextMenu.Item>
+            <ContextMenu.Item>SubContent1</ContextMenu.Item>
+            <ContextMenu.Item>SubContent0</ContextMenu.Item>
+            <ContextMenu.Item>SubContent1</ContextMenu.Item>
+            <ContextMenu.Item>SubContent0</ContextMenu.Item>
+            <ContextMenu.Item>SubContent1</ContextMenu.Item>
+            <ContextMenu.Item>SubContent0</ContextMenu.Item>
+            <ContextMenu.Item>SubContent1</ContextMenu.Item>
+          </ContextMenu.Content>
+        </ContextMenu.Sub>
+        <ContextMenu.Item>Content2</ContextMenu.Item>
+        <ContextMenu.Item disabled>Content3</ContextMenu.Item>
+      </ContextMenu.Content>
+    </ContextMenu>
+  );
+};
+export default {
+  component: ContextMenuExample,
+  title: "Display/ContextMenu",
+  tags: ["form-field", "dropdown", "autodocs"],
+  argTypes: {
+    disabled: { control: "boolean" },
+    showArrow: { control: "boolean" },
+  },
+};
+
+export const Playground = {
+  args: {},
+};
---
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,133 @@
+import * as RightMenu from "@radix-ui/react-context-menu";
+import styled from "styled-components";
+import { Icon } from "..";
+import { Arrow, GenericMenuItem, GenericMenuPanel } from "../GenericMenu";
+import PopoverArrow from "../icons/PopoverArrow";
+
+export const ContextMenu = (props: RightMenu.ContextMenuProps) => (
+  <RightMenu.Root {...props} />
+);
+
+const RightMenuItem = styled(GenericMenuItem)`
+  position: relative;
+  &:hover .dropdown-arrow,
+  &[data-state="open"] .dropdown-arrow {
+    left: 0.5rem;
+  }
+`;
+
+const ContextMenuTrigger = (props: RightMenu.ContextMenuTriggerProps) => {
+  return <RightMenu.Trigger {...props} />;
+};
+
+ContextMenuTrigger.displayName = "ContextMenuTrigger";
+ContextMenu.Trigger = ContextMenuTrigger;
+
+const ContextMenuSubTrigger = ({
+  children,
+  ...props
+}: RightMenu.ContextMenuSubTriggerProps) => {
+  return (
+    <RightMenuItem
+      as={RightMenu.SubTrigger}
+      {...props}
+    >
+      {children}
+      <div className="dropdown-arrow">
+        <Icon name="chevron-right" />
+      </div>
+    </RightMenuItem>
+  );
+};
+
+ContextMenuSubTrigger.displayName = "ContextMenuSubTrigger";
+ContextMenu.SubTrigger = ContextMenuSubTrigger;
+
+export type ArrowProps = {
+  showArrow?: boolean;
+};
+
+type ContextMenuContentProps = RightMenu.MenuContentProps & {
+  sub?: true;
+} & ArrowProps;
+
+type ContextMenuSubContentProps = RightMenu.MenuSubContentProps & {
+  sub?: never;
+} & ArrowProps;
+
+const RightMenuContent = styled(GenericMenuPanel)`
+  flex-direction: column;
+  z-index: 1;
+`;
+
+const ContextMenuContent = ({
+  sub,
+  children,
+  showArrow,
+  ...props
+}: ContextMenuContentProps | ContextMenuSubContentProps) => {
+  const ContentElement = sub ? RightMenu.SubContent : RightMenu.Content;
+  return (
+    <RightMenu.Portal>
+      <RightMenuContent
+        type="context-menu"
+        as={ContentElement}
+        {...props}
+      >
+        {showArrow && (
+          <Arrow
+            asChild
+            as={RightMenu.Arrow}
+            width={20}
+            height={10}
+          >
+            <PopoverArrow />
+          </Arrow>
+        )}
+        {children}
+      </RightMenuContent>
+    </RightMenu.Portal>
+  );
+};
+
+ContextMenuContent.displayName = "ContextMenuContent";
+ContextMenu.Content = ContextMenuContent;
+
+const RightMenuGroup = styled(RightMenu.Group)`
+  width: 100%;
+  border-bottom: 1px solid
+    ${({ theme }) => theme.click.genericMenu.item.color.stroke.default};
+`;
+
+const ContextMenuGroup = (props: RightMenu.ContextMenuGroupProps) => {
+  return <RightMenuGroup {...props} />;
+};
+
+ContextMenuGroup.displayName = "ContextMenuGroup";
+ContextMenu.Group = ContextMenuGroup;
+
+const RightMenuSub = styled(RightMenu.Sub)`
+  border-bottom: 1px solid
+    ${({ theme }) => theme.click.genericMenu.item.color.stroke.default};
+`;
+
+const ContextMenuSub = ({ ...props }: RightMenu.ContextMenuGroupProps) => {
+  return <RightMenuSub {...props} />;
+};
+
+ContextMenuSub.displayName = "ContextMenuSub";
+ContextMenu.Sub = ContextMenuSub;
+
+const ContextMenuItem = ({ ...props }: RightMenu.ContextMenuItemProps) => {
+  return (
+    <RightMenuItem
+      as={RightMenu.Item}
+      {...props}
+    />
+  );
+};
+
+ContextMenuItem.displayName = "ContextMenuItem";
+ContextMenu.Item = ContextMenuItem;
+
+export default ContextMenu;
