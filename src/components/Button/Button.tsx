f3a26257595a403b003f5477141ad42ff2d76472
Sergio de Cristofaro <sdecri@gmail.com>
2023-07-31 14:46:28 +0200

    remove disabled prop, use center as default align value

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { IconName } from "@/components/Icon/types";
 import styled from "styled-components";
 
 type ButtonType = "primary" | "secondary" | "danger";
-type Alignment = "default" | "left";
+type Alignment = "center" | "left";
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   type?: ButtonType;
@@ -26,18 +26,31 @@ export interface StyledButtonProps {
 
 export const Button = ({
   type = "primary",
-  disabled = false,
   iconLeft,
   iconRight,
   label,
-  align,
+  align = "center",
   children,
   ...delegated
 }: ButtonProps) => (
-  <StyledButton styleType={type} align={align} {...delegated} disabled={disabled}>
-    {iconLeft && <Icon name={iconLeft} size="small" />}
+  <StyledButton
+    styleType={type}
+    align={align}
+    {...delegated}
+  >
+    {iconLeft && (
+      <Icon
+        name={iconLeft}
+        size="small"
+      />
+    )}
     {label ? label : children}
-    {iconRight && <Icon name={iconRight} size="small" />}
+    {iconRight && (
+      <Icon
+        name={iconRight}
+        size="small"
+      />
+    )}
   </StyledButton>
 );
 
@@ -45,37 +58,57 @@ const BaseButton = styled.button`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  padding: ${props => props.theme.click.button.basic.space.y} ${props => props.theme.click.button.basic.space.x};
-  border-radius: ${props => props.theme.click.button.radii.all};
-  gap: ${props => props.theme.click.button.basic.space.gap};
+  padding: ${(props) => props.theme.click.button.basic.space.y}
+    ${(props) => props.theme.click.button.basic.space.x};
+  border-radius: ${(props) => props.theme.click.button.radii.all};
+  gap: ${(props) => props.theme.click.button.basic.space.gap};
   cursor: pointer;
 `;
 
-const StyledButton = styled(BaseButton)<Pick<StyledButtonProps, "width" | "height" | "align" | "styleType" >>`
-  width: ${props => props.width};
-  height: ${props => props.height};
-  font: ${props => props.theme.click.button.basic.typography.label.default};
-  color: ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].text.default};
-  background-color: ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].background.default};
-  border: 1px solid ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].stroke.default};
+const StyledButton = styled(BaseButton)<
+  Pick<StyledButtonProps, "width" | "height" | "align" | "styleType">
+>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font: ${(props) => props.theme.click.button.basic.typography.label.default};
+  color: ${({ styleType = "primary", theme }) =>
+    theme.click.button.basic.color[styleType].text.default};
+  background-color: ${({ styleType = "primary", theme }) =>
+    theme.click.button.basic.color[styleType].background.default};
+  border: 1px solid
+    ${({ styleType = "primary", theme }) =>
+      theme.click.button.basic.color[styleType].stroke.default};
   display: flex;
-  align-items: ${({ align }) => align === "left" ? "left" : "center"};
-  justify-content: ${props => props.align === "left" ? "flex-start" : "center"};
+  align-items: ${({ align }) => (align === "left" ? "left" : "center")};
+  justify-content: ${(props) => (props.align === "left" ? "flex-start" : "center")};
 
   &:hover {
-    background-color: ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].background.hover};
-    border: 1px solid ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].stroke.hover};
+    background-color: ${({ styleType = "primary", theme }) =>
+      theme.click.button.basic.color[styleType].background.hover};
+    border: 1px solid
+      ${({ styleType = "primary", theme }) =>
+        theme.click.button.basic.color[styleType].stroke.hover};
   }
 
-  &:active, &:focus {
-    background-color: ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].background.active};
-    border: 1px solid ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].stroke.active};
+  &:active,
+  &:focus {
+    background-color: ${({ styleType = "primary", theme }) =>
+      theme.click.button.basic.color[styleType].background.active};
+    border: 1px solid
+      ${({ styleType = "primary", theme }) =>
+        theme.click.button.basic.color[styleType].stroke.active};
   }
 
-  &:disabled, &:disabled:hover, &:disabled:active {
-    background-color: ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].background.disabled};
-    color: ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].text.disabled};
-    border: 1px solid ${({ styleType = "primary", theme }) => theme.click.button.basic.color[styleType].stroke.disabled};
+  &:disabled,
+  &:disabled:hover,
+  &:disabled:active {
+    background-color: ${({ styleType = "primary", theme }) =>
+      theme.click.button.basic.color[styleType].background.disabled};
+    color: ${({ styleType = "primary", theme }) =>
+      theme.click.button.basic.color[styleType].text.disabled};
+    border: 1px solid
+      ${({ styleType = "primary", theme }) =>
+        theme.click.button.basic.color[styleType].stroke.disabled};
     cursor: not-allowed;
   }
 `;
