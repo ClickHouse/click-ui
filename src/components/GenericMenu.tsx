412c6313edee320a01a111cd521436a295567e60
Vineeth Asok Kumar <me@vineethasok.com>
2023-07-31 15:45:19 +0200

    Add Hover Card primitive (#76)
    * Add Hover Card Styling
    
    * Fix Hover Card
    
    * Fix stories Naming
    
    * Update token

--- src/components/GenericMenu.tsx
+++ src/components/GenericMenu.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const GenericMenuPanel = styled.div<{
-  type: "popover" | "dropdown-menu" | "context-menu";
+  type: "popover" | "dropdown-menu" | "context-menu" | "hover-card";
 }>`
   outline: none;
   max-width: var(--radix-${({ type }) => type}-content-available-width);
@@ -18,9 +18,33 @@ export const GenericMenuPanel = styled.div<{
   `}
 `;
 
+export const GenericPopoverMenuPanel = styled.div<{
+  $type: "popover" | "hover-card";
+  $showArrow?: boolean;
+}>`
+  outline: none;
+  max-width: var(--radix-${({ $type }) => $type}-content-available-width);
+  max-height: var(--radix-${({ $type }) => $type}-content-available-height);
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  z-index: 1;
+
+  ${({ theme }) => `
+    border: 1px solid ${theme.click.popover.color.panel.stroke.default};
+    background: ${theme.click.popover.color.panel.background.default};
+    padding: ${theme.click.popover.space.y} ${theme.click.popover.space.x};
+    border-radius: ${theme.click.popover.radii.all};
+    box-shadow: ${theme.click.popover.shadow.default};
+  `}
+  ${({ $showArrow }) => ($showArrow ? "margin: -1px 0;" : "")};
+`;
+
 export const Arrow = styled.svg`
+  filter: drop-shadow(rgba(0, 0, 0, 0.1) 0px 4px 6px);
   ${({ theme }) => `
     fill: ${theme.click.genericMenu.panel.color.background.default};
     stroke: ${theme.click.genericMenu.panel.color.stroke.default};
-  `}
+  `};
 `;
---
+++ src/components/HoverCard/HoverCard.stories.tsx
@@ -0,0 +1,64 @@
+import { Checkbox } from "..";
+import { Title } from "../Typography/Title/Title";
+import { HoverCard } from "./HoverCard";
+
+const HoverCardComponent = ({
+  open,
+  showArrow,
+  forceMount,
+  title,
+  openDelay,
+  closeDelay,
+}: {
+  open: "default" | "open" | "closed";
+  showArrow: boolean;
+  forceMount?: boolean;
+  title?: string;
+  openDelay?: number;
+  closeDelay?: number;
+}) => (
+  <HoverCard
+    open={open === "default" ? undefined : open === "open"}
+    openDelay={openDelay}
+    closeDelay={closeDelay}
+  >
+    <HoverCard.Trigger>Hover Here</HoverCard.Trigger>
+    <HoverCard.Content
+      showArrow={showArrow}
+      forceMount={forceMount ? true : undefined}
+      title={title}
+    >
+      <Title
+        color="default"
+        size="sm"
+        type="h5"
+      >
+        Hover Title
+      </Title>
+      <div>
+        Click on the input element below
+        <Checkbox />
+        <div>This is a sample data to experiment the popover</div>
+      </div>
+    </HoverCard.Content>
+  </HoverCard>
+);
+
+export default {
+  component: HoverCardComponent,
+  title: "Display/HoverCard",
+  tags: ["form-field", "hover-card", "autodocs"],
+  argTypes: {
+    open: { control: "radio", options: ["default", "open", "closed"] },
+    showArrow: { control: "boolean" },
+    forceMount: { control: "boolean" },
+    openDelay: { control: "number" },
+    closeDelay: { control: "number" },
+  },
+};
+
+export const Playground = {
+  args: {
+    open: "default",
+  },
+};
