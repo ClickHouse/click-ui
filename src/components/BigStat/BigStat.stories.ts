eb7889e5df4385dddd938f5c04e34fad075cbe1c
Gareth Jones <github@garethdjones.com>
2023-08-02 09:53:41 -0400

    Removing erranous content from big-stat story.

--- src/components/BigStat/BigStat.stories.ts
+++ src/components/BigStat/BigStat.stories.ts
@@ -4,14 +4,6 @@ export default {
   component: BigStat,
   title: "Display/Big Stat",
   tags: ["big-stat", "autodocs"],
-  parameters: {
-    docs: {
-      story: {
-        inline: true,
-        iframeHeight: 150,
-      },
-    },
-  },
   size: {
     options: ["lg", "sm"],
     control: { type: "radio" },
