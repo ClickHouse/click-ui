5372d69e41bb4ac577127c05375e6ef592661a96
Vineeth Asok Kumar <me@vineethasok.com>
2023-08-02 11:11:19 +0200

    Add select autocomplete changes (#53)
    * Add select autocomplete changes
    
    * fix incorrect token reference
    
    * Select styling changes
    
    * Fix select autocomplete changes
    
    * Fix types
    
    * Add min height to search input
    
    * Fix types
    
    * Fix select styling changes
    
    * Add Autocomplete changes
    
    * Fix tokens
    
    * Fix select imports
    
    * Fix select errors
    
    * Fix incorrect test case import
    
    * Add token changes
    
    * Fix Select nodata Color
    
    * Fix package json lock file
    
    * Fix select search

--- src/components/FormField/Select.stories.tsx
+++ src/components/FormField/Select.stories.tsx
@@ -1,27 +1,48 @@
-import Select from "./Select";
-
-const children = (
-  <>
-    <Select.Group label='Group label'>
-      <Select.Item value='content0'>Content0</Select.Item>
-    </Select.Group>
-    <Select.Separator />
-    <Select.Item value='content1'>Content1 long text content</Select.Item>
-    <Select.Separator />
-    <Select.Item value='content2'>Content2</Select.Item>
-    <Select.Item value='content3'>Content3</Select.Item>
-  </>
-);
-
+import { Select, SelectProps } from "./Select";
+interface Props extends SelectProps {
+  clickableNoData?: boolean;
+}
+const SelectExample = ({ clickableNoData, ...props }: Props) => {
+  return (
+    <Select {...props}>
+      <Select.Group heading="Group label">
+        <Select.Item value="content0">Content0</Select.Item>
+      </Select.Group>
+      <Select.Item value="content1">Content1 long text content</Select.Item>
+      <Select.Item value="content2">Content2</Select.Item>
+      <Select.Item value="content3">Content3</Select.Item>
+      {clickableNoData ? (
+        <Select.NoData onClick={() => console.log("Asasas")}>
+          {({ search }) => `No Field found ${search}`}
+        </Select.NoData>
+      ) : (
+        <Select.NoData />
+      )}
+    </Select>
+  );
+};
 export default {
-  component: Select,
+  component: SelectExample,
   title: "Forms/Select",
   tags: ["form-field", "select", "autodocs"],
+  argTypes: {
+    label: { control: "string" },
+    disabled: { control: "boolean" },
+    error: { control: "string" },
+    value: { control: "string" },
+    defaultValue: { control: "string" },
+    open: { control: "inline-radio", options: [undefined, true, false] },
+    defaultOpen: { control: "boolean" },
+    name: { control: "string" },
+    required: { control: "boolean" },
+    showSearch: { control: "boolean" },
+    isFormCotrol: { control: "boolean" },
+    clickableNoData: { control: "boolean" },
+  },
 };
 
 export const Playground = {
   args: {
     label: "Label",
-    children,
   },
 };
