c7ab727aad620bd272788d3e68e10e7c37890080
Sergio de Cristofaro <sdecri@gmail.com>
2023-08-08 10:57:08 +0200

    restore resolve path

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "styled-components": path.resolve("./node_modules/styled-components"),
     },
   },
   build: {
