c0e598d9eb537480307f1f9d5c618c9ce4253bb2
Vineeth Asok Kumar <me@vineethasok.com>
2023-08-08 13:16:43 +0200

    Add new Input element (#55)
    * Add Input Element
    
    * Fix Input styling
    
    * Add Input element styling
    
    * Reformat code
    
    * Add placeholder color for input
    
    * Fix Color and font for input
    
    * Fix styling
    
    * Add id to elements
    
    * Fix Label
    
    * Optimize input code
    
    * Optimize input code
    
    * Optimize Input component
    
    * Fix incorrect token assignment
    
    * Remove radix-select
    
    * Add Input changes
    
    * Fix Input styling changes
    
    * Fix Styling
    
    * Remove optional for input
    
    * Update onChange event in Input
    
    * Update stories to have only necessary values
    
    * Update input btn styling
    
    * Fix tests
    
    * Fix lint issues
    
    * Fix Number Field storybook naming

---
+++ src/components/FormField/Input/NumberField.stories.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { NumberField as NumberFieldInput, NumberInputProps } from "./NumberField";
+
+const NumberField = ({
+  value: valueProp,
+  ...props
+}: Omit<NumberInputProps, "onChange">) => {
+  const [value, setValue] = useState(valueProp);
+  useEffect(() => {
+    setValue(valueProp);
+  }, [valueProp]);
+
+  return (
+    <NumberFieldInput
+      value={value}
+      onChange={(inputValue: string) => {
+        setValue(inputValue);
+      }}
+      {...props}
+    />
+  );
+};
+
+export default {
+  component: NumberField,
+  title: "Forms/Input/NumberField",
+  tags: ["form-field", "input", "autodocs"],
+  argTypes: {
+    value: { control: "text" },
+    label: { control: "text" },
+    error: { control: "text" },
+    disabled: { control: "boolean" },
+    placeholder: { control: "text" },
+    readOnly: { control: "boolean" },
+  },
+};
+
+export const Playground = {
+  args: {
+    label: "Label",
+    disabled: false,
+    placeholder: "Placeholder",
+  },
+};
---
+++ src/components/FormField/Input/TextField.stories.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { TextField as TextFieldInput, TextInputProps } from "./TextField";
+
+const TextField = ({ value: valueProp, ...props }: Omit<TextInputProps, "onChange">) => {
+  const [value, setValue] = useState(valueProp);
+  useEffect(() => {
+    setValue(valueProp);
+  }, [valueProp]);
+
+  return (
+    <TextFieldInput
+      value={value}
+      onChange={(inputValue: string, e?: ChangeEvent<HTMLInputElement>) => {
+        if (e) {
+          e.preventDefault();
+        }
+        setValue(inputValue);
+      }}
+      {...props}
+    />
+  );
+};
+
+export default {
+  component: TextField,
+  title: "Forms/Input/TextField",
+  tags: ["form-field", "input", "autodocs"],
+  argTypes: {
+    type: {
+      control: "inline-radio",
+      options: ["text", "email", "tel", "url"],
+    },
+    value: { control: "text" },
+    clear: { control: "boolean" },
+    label: { control: "text" },
+    error: { control: "text" },
+    disabled: { control: "boolean" },
+    placeholder: { control: "text" },
+    readOnly: { control: "boolean" },
+  },
+};
+
+export const Playground = {
+  args: {
+    label: "Label",
+    clear: false,
+    type: "text",
+    disabled: false,
+    placeholder: "Placeholder",
+  },
+};
