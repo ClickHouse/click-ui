3c00c09918ec0cb8341b2da496de66f19deaa0ac
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-08-11 17:29:44 +0200

    Add File Tabs changes

--- src/styles/types.ts
+++ src/styles/types.ts
@@ -1414,6 +1414,12 @@
               "gap": string
             }
           },
+          "icon": {
+            "size": {
+              "height": string,
+              "width": string
+            }
+          },
           "color": {
             "background": {
               "active": string,
@@ -1737,6 +1743,46 @@
           }
         }
       },
+      "fileTabs": {
+        "icon": {
+          "size": {
+            "height": string,
+            "width": string
+          }
+        },
+        "space": {
+          "y": string,
+          "x": string,
+          "gap": string
+        },
+        "typography": {
+          "label": {
+            "default": string,
+            "hover": string,
+            "active": string
+          }
+        },
+        "radii": {
+          "all": string
+        },
+        "color": {
+          "background": {
+            "default": string,
+            "hover": string,
+            "active": string
+          },
+          "text": {
+            "default": string,
+            "hover": string,
+            "active": string
+          },
+          "stroke": {
+            "default": string,
+            "hover": string,
+            "active": string
+          }
+        }
+      },
       "file-tabs": {
         "color": {
           "background": {
@@ -1856,6 +1902,28 @@
         },
         "opaqueBackground": {
           "default": string
+        }
+      }
+    },
+    "link": {
+      "space": {
+        "md": {
+          "gap": string
+        },
+        "sm": {
+          "gap": string
+        }
+      },
+      "icon": {
+        "size": {
+          "sm": {
+            "height": string,
+            "width": string
+          },
+          "md": {
+            "height": string,
+            "width": string
+          }
         }
       }
     },
