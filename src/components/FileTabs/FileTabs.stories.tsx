2569e691b1262dd14f1759f5ae7ea3fe410da458
Vineeth Asok Kumar <me@vineethasok.com>
2023-08-16 11:03:34 +0200

    Add FileTabs Components (#94)
    * Add FileTabs changes
    
    * Fix File tabs behaviour
    
    * Add File Tabs changes
    
    * Add FileTabs Changes
    
    * Fix lint errors
    
    * Add FileTabs changes
    
    * Fix File tabs behaviour
    
    * Add File Tabs changes
    
    * Add FileTabs Changes
    
    * Fix lint errors
    
    * optimize filetabs
    
    * Update tokens
    
    * Add test cases
    
    * Remove unwanted import
    
    * Fix lint error
    
    * Fix test cases

---
+++ src/components/FileTabs/FileTabs.stories.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { FileTabs as CUIFileTabs, StatusType } from "./FileTabs";
+
+const options = [
+  "a1",
+  "a2",
+  "a3",
+  "a4",
+  "a5",
+  "a6",
+  "a7",
+  "a8",
+  "a9",
+  "a10",
+  "a11",
+  "a12",
+];
+
+const FileTabs = ({
+  selected: selectedProp,
+  status,
+}: {
+  selected: string;
+  status: StatusType;
+}) => {
+  const [selected, setSelected] = useState(selectedProp);
+  useEffect(() => {
+    setSelected(selectedProp);
+  }, [selectedProp]);
+  const [tabs, setTabs] = useState(options);
+
+  return (
+    <CUIFileTabs
+      onReorderTab={() => null}
+      onClose={(_: string, index: number) => {
+        setTabs(tabs => {
+          tabs.splice(index, 1);
+          return [...tabs];
+        });
+      }}
+      onSelect={(value: string) => setSelected(value)}
+      selected={selected}
+    >
+      {tabs.map((option, index) => (
+        <CUIFileTabs.Tab
+          value={option}
+          key={`${option}-${index}`}
+          icon="code-in-square"
+          status={index === 0 ? status : undefined}
+          text={`Tab ${index} value-${option}`}
+        />
+      ))}
+    </CUIFileTabs>
+  );
+};
+export default {
+  component: FileTabs,
+  title: "Display/FileTabs",
+  tags: ["tabs", "file-tabs", "autodocs"],
+  argTypes: {
+    selected: { control: "select", options },
+    status: {
+      control: "radio",
+      options: ["success", "warning", "danger", "neutral", "info"],
+    },
+  },
+};
+
+export const Playground = {
+  args: {},
+};
--- src/styles/types.ts
+++ src/styles/types.ts
@@ -482,12 +482,6 @@
           }
         },
         "primary": {
-          "stroke": {
-            "default": string,
-            "active": string,
-            "hover": string,
-            "disabled": string
-          },
           "background": {
             "main": {
               "default": string,
@@ -508,10 +502,16 @@
             "active": string,
             "disabled": string
           },
+          "stroke": {
+            "default": string,
+            "hover": string,
+            "active": string,
+            "disabled": string
+          },
           "divide": {
             "default": string,
-            "active": string,
             "hover": string,
+            "active": string,
             "disabled": string
           }
         }
@@ -1414,6 +1414,12 @@
               "gap": string
             }
           },
+          "icon": {
+            "size": {
+              "height": string,
+              "width": string
+            }
+          },
           "color": {
             "background": {
               "active": string,
@@ -1737,7 +1743,28 @@
           }
         }
       },
-      "file-tabs": {
+      "fileTabs": {
+        "icon": {
+          "size": {
+            "height": string,
+            "width": string
+          }
+        },
+        "space": {
+          "y": string,
+          "x": string,
+          "gap": string
+        },
+        "typography": {
+          "label": {
+            "default": string,
+            "hover": string,
+            "active": string
+          }
+        },
+        "radii": {
+          "all": string
+        },
         "color": {
           "background": {
             "default": string,
@@ -1856,6 +1883,28 @@
         },
         "opaqueBackground": {
           "default": string
+        }
+      }
+    },
+    "link": {
+      "space": {
+        "md": {
+          "gap": string
+        },
+        "sm": {
+          "gap": string
+        }
+      },
+      "icon": {
+        "size": {
+          "sm": {
+            "height": string,
+            "width": string
+          },
+          "md": {
+            "height": string,
+            "width": string
+          }
         }
       }
     },
