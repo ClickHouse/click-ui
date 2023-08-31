c14fc1dbe67c9bd84d0455482ca98ff5d5760702
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-08-31 12:38:27 +0200

    Optimize Build token code

--- build-tokens.js
+++ build-tokens.js
@@ -1,17 +1,17 @@
-import _ from "lodash";
+import setWith from "lodash/setWith";
 import { registerTransforms, transforms } from "@tokens-studio/sd-transforms";
 import StyleDictionary from "style-dictionary";
 
 registerTransforms(StyleDictionary);
 const themes = ["classic", "dark", "light"];
 
-function generateThemeFromDictionary(dictionary, valueFunc = (value) => value) {
+const generateThemeFromDictionary = (dictionary, valueFunc = value => value) => {
   const theme = {};
-  dictionary.allTokens.forEach((token) => {
-    _.setWith(theme, token.name, valueFunc(token.value), Object)
+  dictionary.allTokens.forEach(token => {
+    setWith(theme, token.name, valueFunc(token.value), Object)
   });
   return theme;
-}
+};
 
 StyleDictionary.registerTransform({
   type: "name",
@@ -27,7 +27,7 @@ StyleDictionary.registerTransform({
 
 StyleDictionary.registerFormat({
   name: "ThemeFormat",
-  formatter: function ({ dictionary, platform, options, file }) {
+  formatter: ({ dictionary }) => {
     const theme = generateThemeFromDictionary(dictionary);
     return JSON.stringify(theme, null, 2);
   }
@@ -35,8 +35,8 @@ StyleDictionary.registerFormat({
 
 StyleDictionary.registerFormat({
   name: "TypescriptFormat",
-  formatter: function ({ dictionary, platform, options, file }) {
-    const theme = generateThemeFromDictionary(dictionary, (value) => typeof value);
+  formatter: ({ dictionary }) => {
+    const theme = generateThemeFromDictionary(dictionary, value => typeof value);
 
     return `
       export interface Theme ${JSON.stringify(theme, null, 2).replaceAll("\"string\"", "string").replaceAll("\"number\"", "number")}
