f73eed5edb629eef4b8611a988e0eb3c522761ce
Gareth Jones <github@garethdjones.com>
2023-09-02 13:59:56 -0400

    Fixing the casing

--- src/components/icons/Download.tsx
+++ src/components/icons/Download.tsx
@@ -12,29 +12,29 @@ const Download = (props: SVGAttributes<SVGElement>) => (
     <path
       stroke="#161517"
       strokeWidth={1.5}
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M7 6H4.538C3.689 6 3 6.689 3 7.538V19.23C3 20.08 3.689 20.768 4.538 20.768H19.461C20.311 20.768 20.999 20.079 20.999 19.23V7.538C21 6.689 20.311 6 19.462 6H17"
     />
     <path
       stroke="#161517"
       strokeWidth={1.5}
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M15 10L12 13L9 10"
     />
     <path
       stroke="#161517"
       strokeWidth={1.5}
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M12 3V13"
     />
     <path
       stroke="#161517"
       strokeWidth={1.5}
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M8 16H16"
     />
   </svg>
