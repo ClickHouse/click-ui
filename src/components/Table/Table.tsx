d2412fdc3f0479b1024f5978de9a0d3bf1145584
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-11-06 17:26:22 +0100

    Optimize table code and make it responsive

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,18 +1,20 @@
-import { Icon, IconName } from "@/components";
-import { HTMLAttributes, forwardRef } from "react";
+import { HorizontalDirection, Icon, IconName } from "@/components";
+import { HTMLAttributes, ReactNode, forwardRef } from "react";
 import styled from "styled-components";
 
-type TableHeaderProps = {
-  children: React.ReactNode;
-  leftIcon?: IconName;
-  rightIcon?: IconName;
-};
-type TableRowType = "header" | "default";
+interface TableHeaderProps extends HTMLAttributes<HTMLTableCellElement> {
+  icon?: IconName;
+  iconDir?: HorizontalDirection;
+  label: ReactNode;
+}
 const StyledHeader = styled.th`
-  padding: 0.75rem 1rem;
-  font: ${props => props.theme.click.table.header.title.default};
-  color: ${props => props.theme.click.table.header.color.title.default};
-  gap: 4px;
+  ${({ theme }) => `
+    padding: ${theme.click.table.header.cell.space.md.y}
+      ${theme.click.table.body.cell.space.md.x};
+    font: ${theme.click.table.header.title.default};
+    color: ${theme.click.table.header.color.title.default};
+  `}
+  gap: 0.25rem;
   text-align: left;
 `;
 
@@ -20,81 +22,140 @@ const HeaderContentWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: start;
-  gap: 4px;
+  gap: inherit;
 `;
 
 const TableHeader = ({
-  leftIcon,
-  rightIcon,
-  children,
+  icon,
+  iconDir = "end",
+  label,
   ...delegated
 }: TableHeaderProps) => (
   <StyledHeader {...delegated}>
     <HeaderContentWrapper>
-      {leftIcon && (
+      {icon && iconDir == "start" && (
         <Icon
-          name={leftIcon}
-          width="12px"
-          height="12px"
+          name={icon}
+          size="sm"
         />
       )}
-      {children}
-      {rightIcon && (
+      {label}
+      {icon && iconDir == "end" && (
         <Icon
-          name={rightIcon}
-          width="12px"
-          height="12px"
+          name={icon}
+          size="sm"
         />
       )}
     </HeaderContentWrapper>
   </StyledHeader>
 );
 
-const StyledTableRow = styled.tr<{ $type?: TableRowType }>`
+const TableRow = styled.tr`
   overflow: hidden;
-  ${({ $type = "default", theme }) =>
-    $type === "default"
-      ? `
-        background-color: ${theme.click.table.row.color.background.default};
-        border-bottom: 1px solid ${theme.click.table.row.color.stroke.default};
-        &:active {
-          background-color: ${theme.click.table.row.color.background.active};
-        }
-        &:hover {
-          background-color: ${theme.click.table.row.color.background.hover};
-        }
-      `
-      : `background-color: ${theme.click.table.header.color.background.default};`}
+  ${({ theme }) => `
+    background-color: ${theme.click.table.row.color.background.default};
+    border-bottom: 1px solid ${theme.click.table.row.color.stroke.default};
+    &:active {
+      background-color: ${theme.click.table.row.color.background.active};
+    }
+    &:hover {
+      background-color: ${theme.click.table.row.color.background.hover};
+    }
+  `}
 
   &:last-of-type {
     border-bottom: none;
   }
-`;
 
-const TableRow = ({
-  type,
-  ...props
-}: HTMLAttributes<HTMLTableRowElement> & { type?: TableRowType }) => {
-  return (
-    <StyledTableRow
-      $type={type}
-      {...props}
-    />
-  );
-};
+  @media (max-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    ${({ theme }) => `
+      border: 1px solid ${theme.click.table.row.color.stroke.default};
+      border-radius: ${theme.click.table.radii.all};
+    `}
+  }
+`;
 
 const TableData = styled.td`
-  font: ${props => props.theme.click.table.cell.label.default};
-  padding: 0.75rem 1rem;
   overflow: hidden;
+  ${({ theme }) => `
+    color: ${theme.click.table.row.color.text.default};
+    font: ${theme.click.table.cell.text.default};
+    padding: ${theme.click.table.body.cell.space.md.y} ${theme.click.table.body.cell.space.md.x};
+  `}
+  @media (max-width: 768px) {
+    width: auto;
+    min-width: 40%;
+    padding: ${({ theme }) => theme.click.table.body.cell.space.sm.y}
+      ${({ theme }) => theme.click.table.body.cell.space.sm.x};
+  }
 `;
 
-const Table = forwardRef<HTMLTableElement, HTMLAttributes<HTMLTableElement>>(
-  (props, ref) => (
+const THead = styled.thead`
+  tr {
+    overflow: hidden;
+    background-color: ${({ theme }) => theme.click.table.header.color.background.default};
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileHeader = styled.span`
+  display: none;
+  ${({ theme }) => `
+    color: ${theme.click.table.row.color.label.default};
+    font:  ${theme.click.table.cell.label.default};
+  `}
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+const Tbody = styled.tbody`
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+  }
+`;
+interface TableCellType extends HTMLAttributes<HTMLTableCellElement> {
+  label: ReactNode;
+}
+type TableRowType = Array<TableCellType>;
+interface TableProps extends HTMLAttributes<HTMLTableElement> {
+  headers: Array<TableHeaderProps>;
+  rows: Array<TableRowType>;
+}
+const Table = forwardRef<HTMLTableElement, TableProps>(
+  ({ headers, rows, ...props }, ref) => (
     <StyledTable
       ref={ref}
       {...props}
-    />
+    >
+      <THead>
+        <tr>
+          {headers.map((headerProps, index) => (
+            <TableHeader
+              key={`table-header-${index}`}
+              {...headerProps}
+            />
+          ))}
+        </tr>
+      </THead>
+      <Tbody>
+        {rows.map(row => (
+          <TableRow>
+            {row.map(({ label, ...cellProps }, index) => (
+              <TableData {...cellProps}>
+                {headers[index] && <MobileHeader>{headers[index].label}</MobileHeader>}
+                <span>{label}</span>
+              </TableData>
+            ))}
+          </TableRow>
+        ))}
+      </Tbody>
+    </StyledTable>
   )
 );
 
@@ -105,17 +166,10 @@ const StyledTable = styled.table`
     border-radius: ${theme.click.table.radii.all};
     border: 1px solid ${theme.click.table.global.color.stroke.default};
   `}
+
+  @media (max-width: 768px) {
+    border: none;
+  }
 `;
 
-TableHeader.displayName = "Table.Th";
-
-TableData.displayName = "Table.Td";
-
-TableRow.displayName = "Table.Tr";
-
-const TableNamespace = Object.assign(Table, {
-  Th: TableHeader,
-  Td: TableData,
-  Tr: TableRow,
-});
-export { TableNamespace as Table };
+export { Table };
