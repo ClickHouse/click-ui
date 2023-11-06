fba4455d7b3e79c2f9f0f217d19387c5d6603e74
Vineeth Asok Kumar <me@vineethasok.com>
2023-11-06 17:52:35 +0100

    Fix Table Styling (#196)
    * Fix Table Styling
    
    * Optimize table code and make it responsive

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,5 +1,15 @@
 import { Table } from "./Table";
 
+const headers = [{ label: "Company" }, { label: "Contact" }, { label: "Country" }];
+const rows = [
+  [{ label: "Alfreds Futterkiste" }, { label: "Maria Anders" }, { label: "Germany" }],
+  [
+    { label: "Centro comercial Moctezuma" },
+    { label: "Francisco Chang" },
+    { label: "Mexico" },
+  ],
+];
+
 export default {
   component: Table,
   title: "Display/Table",
@@ -8,22 +18,7 @@ export default {
 
 export const Playground = {
   args: {
-    children: [
-      <Table.Tr>
-        <Table.Th>Company</Table.Th>
-        <Table.Th>Contact</Table.Th>
-        <Table.Th>Country</Table.Th>
-      </Table.Tr>,
-      <Table.Tr>
-        <Table.Td>Alfreds Futterkiste</Table.Td>
-        <Table.Td>Maria Anders</Table.Td>
-        <Table.Td>Germany</Table.Td>
-      </Table.Tr>,
-      <Table.Tr>
-        <Table.Td>Centro comercial Moctezuma</Table.Td>
-        <Table.Td>Francisco Chang</Table.Td>
-        <Table.Td>Mexico</Table.Td>
-      </Table.Tr>,
-    ],
+    headers,
+    rows,
   },
 };
