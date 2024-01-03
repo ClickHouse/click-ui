55e978d930472caacf24f9a25f3d50446429bbe6
Vineeth Asok Kumar <me@vineethasok.com>
2024-01-03 21:32:03 +0530

    Add Styling fix for collapsible (#286)
    * Add Styling fix for collapsible
    
    * Add tests

--- src/components/SidebarCollapsibleItem/SidebarCollapsibleItem.test.tsx
+++ src/components/SidebarCollapsibleItem/SidebarCollapsibleItem.test.tsx
@@ -3,11 +3,31 @@ import { SidebarCollapsibleItem } from "./SidebarCollapsibleItem";
 import { renderCUI } from "@/utils/test-utils";
 
 describe("SidebarCollapsibleItem", () => {
-  it("should trigger toggle on clicking trigger", () => {
+  it("should trigger toggle on clicking trigger type main", () => {
     const onOpenChange = jest.fn();
     const { queryByTestId } = renderCUI(
       <SidebarCollapsibleItem
         icon="user"
+        label="collapsible item"
+        onOpenChange={onOpenChange}
+        data-testid="collapsible-header"
+      >
+        <div data-testid="collapsible-content">Sidebar nav content</div>
+      </SidebarCollapsibleItem>
+    );
+    const collapsibleHeader = queryByTestId("collapsible-header");
+    expect(collapsibleHeader).not.toBeNull();
+    expect(queryByTestId("collapsible-content")).toBeNull();
+    collapsibleHeader && fireEvent.click(collapsibleHeader);
+    expect(onOpenChange).toBeCalledTimes(1);
+    expect(queryByTestId("collapsible-content")).not.toBeNull();
+  });
+  it("should trigger toggle on clicking trigger type sqlSidebar", () => {
+    const onOpenChange = jest.fn();
+    const { queryByTestId } = renderCUI(
+      <SidebarCollapsibleItem
+        icon="user"
+        type="sqlSidebar"
         label="collapsible item"
         onOpenChange={onOpenChange}
         data-testid="collapsible-header"
