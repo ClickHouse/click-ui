68b1e33b95e3ef4b23472970b86fb26bbf01456b
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2024-01-19 18:09:58 +0100

    Fix Separator

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -4,16 +4,15 @@ interface Props extends RadixSeparator.SeparatorProps {
   size: "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
 }
 const CUISeparator = styled(RadixSeparator.Root)<Props>`
-  &[data-orientation="horizontal"] {
-    height: 0.0625rem;
-    width: 100%;
-  }
-  &[data-orientation="vertical"] {
-    height: 100%;
-    width: 0.0625rem;
-  }
   ${({ theme, size, orientation }) => `
-    background: ${theme.click.separator.color.stroke.default};
+    &[data-orientation="horizontal"] {
+      width: 100%;
+      border-top: 0.0625rem solid ${theme.click.separator.color.stroke.default};
+    }
+    &[data-orientation="vertical"] {
+      height: 100%;
+      border-right: 0.0625rem solid ${theme.click.separator.color.stroke.default};
+    }
     margin: ${
       orientation === "horizontal"
         ? `${theme.click.separator.horizontal.space.y[size]} ${theme.click.separator.horizontal.space.x.all}`
@@ -23,7 +22,10 @@ const CUISeparator = styled(RadixSeparator.Root)<Props>`
 `;
 
 const Separator = ({ orientation = "horizontal", ...props }: Props) => (
-  <CUISeparator orientation={orientation} {...props} />
+  <CUISeparator
+    orientation={orientation}
+    {...props}
+  />
 );
 
 export default Separator;
