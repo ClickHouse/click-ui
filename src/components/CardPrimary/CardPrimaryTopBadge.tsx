1a7ff9573e48aedd3b49500fce846d4e93fc8d64
Sergio de Cristofaro <sdecri@gmail.com>
2024-05-22 12:02:26 +0200

    refactor files

---
+++ src/components/CardPrimary/CardPrimaryTopBadge.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@/components/Badge/Badge";
+import styled from "styled-components";
+
+export const TopBadgeWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 100%;
+`;
+
+export const CardPrimaryTopBadge = styled(Badge)<{ $isSelected?: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  ${({ $isSelected, theme }) =>
+    $isSelected
+      ? `border-color: ${theme.click.button.basic.color.primary.stroke.active};`
+      : ""}
+  div:active + & {
+    border-color: ${({ theme }) => theme.click.button.basic.color.primary.stroke.active};
+  }
+`;
--- src/components/CardPrimary/withTopBadge.tsx
+++ src/components/CardPrimary/withTopBadge.tsx
@@ -1,45 +1,27 @@
-import { Badge } from "@/components/Badge/Badge";
+import {
+  CardPrimaryTopBadge,
+  TopBadgeWrapper,
+} from "@/components/CardPrimary/CardPrimaryTopBadge";
 import { ComponentType, FC } from "react";
-import styled from "styled-components";
 
 export interface WithTopBadgeProps {
   topBadgeText?: string;
   isSelected?: boolean;
 }
 
-const Wrapper = styled.div`
-  position: relative;
-  width: 100%;
-  max-width: 100%;
-`;
-
-const TopBadge = styled(Badge)<{ $isSelected?: boolean }>`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  ${({ $isSelected, theme }) =>
-    $isSelected
-      ? `border-color: ${theme.click.button.basic.color.primary.stroke.active};`
-      : ""}
-  div:active + & {
-    border-color: ${({ theme }) => theme.click.button.basic.color.primary.stroke.active};
-  }
-`;
-
 export const withTopBadge =
   <P extends object>(Component: ComponentType<P>): FC<P & WithTopBadgeProps> =>
   ({ topBadgeText, ...props }: P & WithTopBadgeProps) => {
     return (
-      <Wrapper>
+      <TopBadgeWrapper>
         <Component {...(props as P)} />
         {topBadgeText && (
-          <TopBadge
+          <CardPrimaryTopBadge
             data-testid="card-top-badge"
             text={topBadgeText}
             $isSelected={props.isSelected || false}
           />
         )}
-      </Wrapper>
+      </TopBadgeWrapper>
     );
   };
