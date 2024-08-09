9ac3ca1c2aa19006753ac03946dde8f016127539
Bucky Schwarz <d.w.schwarz@gmail.com>
2024-08-09 12:19:24 -0400

    Add tests

---
+++ src/components/DateTime/DateTime.test.tsx
@@ -0,0 +1,60 @@
+import { DateTime } from "@/components/DateTime/DateTime";
+import { renderCUI } from "@/utils/test-utils";
+import { fireEvent } from "@testing-library/dom";
+
+describe("DateTime", () => {
+  const baseDate = new Date("2024-07-04 11:45:00 AM");
+  const systemTimeZone = "America/Los_Angeles";
+  const locale = new Intl.Locale("en", { region: "US" });
+  const actualTZ = process.env.TZ;
+
+  beforeAll(() => {
+    global.ResizeObserver = vi.fn(() => {
+      return {
+        observe: vi.fn(),
+        unobserve: vi.fn(),
+        disconnect: vi.fn(),
+      };
+    });
+
+    process.env.TZ = "America/New_York";
+  });
+
+  afterAll(() => {
+    process.env.TZ = actualTZ;
+  });
+
+  beforeEach(() => {
+    vi.setSystemTime(baseDate);
+  });
+
+  it("renders the DateTime component with relevant timezone information", () => {
+    const fiveMinutesAgo = new Date("2024-07-04 11:40:00 AM");
+
+    const { getByText } = renderCUI(
+      <DateTime
+        date={fiveMinutesAgo}
+        locale={locale}
+        systemTimeZone={systemTimeZone}
+      />
+    );
+
+    const trigger = getByText("5 minutes ago");
+    expect(trigger).toBeInTheDocument();
+
+    fireEvent.click(trigger);
+    expect(
+      getByText(content => {
+        return content.startsWith("Local (America/New_York");
+      })
+    ).toBeInTheDocument();
+    expect(
+      getByText(content => {
+        return content.startsWith("System (America/Los_Angeles)");
+      })
+    ).toBeInTheDocument();
+    expect(getByText("Jul 4, 2024, 3:40:00 PM")).toBeInTheDocument();
+    expect(getByText("Jul 4, 2024, 11:40:00 AM")).toBeInTheDocument();
+    expect(getByText("Jul 4, 2024, 8:40:00 AM")).toBeInTheDocument();
+  });
+});
