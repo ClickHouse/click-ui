0c4fce3f94229383a88717251426b099b0bc3df6
Shauli Bracha <shauli.bracha@clickhouse.com>
2024-08-22 19:18:40 -0400

    Move Panel types to types file

---
+++ src/components/Panel/types.ts
@@ -0,0 +1,3 @@
+export type PanelPadding = "none" | "xs" | "sm" | "md" | "lg" | "xl";
+export type PanelColor = "default" | "muted" | "transparent";
+export type PanelRadii = "none" | "sm" | "md" | "lg";
