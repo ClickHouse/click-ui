c315ea4502c951021b442a41e1a76d13955e7649
Gareth Jones <github@garethdjones.com>
2024-08-22 21:26:21 -0400

    Adding backgrounds to icons

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -3,14 +3,22 @@ import { FlagList } from "../icons/Flags";
 import { PaymentList } from "../icons/Payments";
 import { Icon } from "./Icon";
 import { ICONS_MAP } from "./IconCommon";
+import { IconProps } from "./types";
+import { Container } from "../Container/Container";
 
 const IconNames = Object.keys(ICONS_MAP);
 const FlagNames = Object.keys(FlagList);
 const LogoNames = Object.keys(LogosLight);
 const PaymentNames = Object.keys(PaymentList);
 
+const IconWrapper = (props: IconProps) => (
+  <Container>
+    <Icon {...props} />
+  </Container>
+);
+
 export default {
-  component: Icon,
+  component: IconWrapper,
   title: "Display/Icon",
   tags: ["icon", "autodocs"],
   argTypes: {
@@ -19,7 +27,11 @@ export default {
       control: { type: "select" },
     },
     size: {
-      options: ["xs" , "sm" , "md" , "lg" , "xl" , "xxl"],
+      options: ["xs", "sm", "md", "lg", "xl", "xxl"],
+      control: { type: "select" },
+    },
+    state: {
+      options: ["default", "info", "success", "warning", "danger", "neutral"],
       control: { type: "select" },
     },
   },
@@ -29,6 +41,7 @@ export const Playground = {
   args: {
     name: "users",
     size: "md",
+    state: "default",
     width: "",
     height: "",
   },
