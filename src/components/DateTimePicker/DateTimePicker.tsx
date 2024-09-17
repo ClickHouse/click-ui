43310fd1770f46826619c2c3c1ee96b3762e4816
Bucky Schwarz <d.w.schwarz@gmail.com>
2024-09-17 17:05:32 -0400

    first bits of styling

--- src/components/DateTimePicker/DateTimePicker.tsx
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -4,23 +4,84 @@ import Dropdown from "../Dropdown/Dropdown";
 import { Icon } from "../Icon/Icon";
 import { InputElement, InputWrapper } from "../Input/InputWrapper";
 import { Container } from "../Container/Container";
+import styled from "styled-components";
 
-const weekdayFormatter = new Intl.DateTimeFormat(undefined, { weekday: "short" });
-const headerDateFormatter = new Intl.DateTimeFormat(undefined, {
+const locale = 'en-US';
+const weekdayFormatter = new Intl.DateTimeFormat(locale, { weekday: "short" });
+const headerDateFormatter = new Intl.DateTimeFormat(locale, {
   month: "short",
   year: "numeric",
 });
-const selectedDateFormatter = new Intl.DateTimeFormat(undefined, {
+const selectedDateFormatter = new Intl.DateTimeFormat(locale, {
   day: "2-digit",
   month: "short",
   year: "numeric"
 })
 
-const DateTimeInputField = ({ id, selectedDate }) => {
+// ${({ theme }) => {
+//     return `
+//       ${theme.global.color.accent.default}
+//     `
+//   }}
+
+//border: 1px solid ${({ theme }) => theme.palette.slate}
+
+const DateTable = styled.table`
+  border-collapse: separate;
+  border-spacing: 0;
+  font: ${({ theme }) => theme.typography.styles.product.text.normal.md }
+  table-layout: fixed;
+  width: 250px;
+
+  tbody {
+    cursor: pointer;
+  }
+
+  th {
+    width: 14%;
+  }
+
+  td {
+    text-align: center;
+  }
+
+  td, th {
+    border: 1px solid transparent;
+    padding: ${({ theme }) => {
+      console.log(theme.click.table.body.cell.space.md);
+      // return theme.click.table.body.cell.space.sm.x;
+      return '4px;'
+    }}
+  }
+`;
+
+const DateTableBody = styled.tbody`
+  cursor: pointer;
+`
+
+const DateTableCell = styled.td<{ $isSelected?: boolean; $isToday?: boolean }>`
+  ${({ $isSelected }) => $isSelected && `background: black; color: white;`}
+  border-radius: ${({ theme }) => theme.border.radii[1]};
+
+  ${({ $isToday }) => $isToday && 'font-weight: bold;'}
+
+  &:hover {
+    border: 1px solid black;
+  }
+`;
+
+interface DateTimeInputFieldProps {
+  disabled: boolean;
+  id?: string;
+  selectedDate?: Date;
+}
+
+const DateTimeInputField = ({ disabled, id, selectedDate }: DateTimeInputFieldProps) => {
   const defaultId = useId();
   const formattedSelectedDate = selectedDate ? selectedDateFormatter.format(selectedDate) : '';
   return (
     <InputWrapper
+      disabled={disabled}
       id={id ?? defaultId}
     >
       <Icon name="calendar" />
@@ -49,7 +110,7 @@ const Calendar = ({ closeDatepicker, selectedDate, setSelectedDate }) => {
 
   return (
     <Container
-      fillWidth
+      fillWidth={false}
       orientation="vertical"
       padding="sm"
     >
@@ -69,7 +130,7 @@ const Calendar = ({ closeDatepicker, selectedDate, setSelectedDate }) => {
           onClick={handleNextClick}
         />
       </Container>
-      <table>
+      <DateTable>
         <thead>
           <tr>
             {headers.weekDays.map(({ key, value: date }) => {
@@ -77,43 +138,43 @@ const Calendar = ({ closeDatepicker, selectedDate, setSelectedDate }) => {
             })}
           </tr>
         </thead>
-        <tbody>
+        <DateTableBody>
           {body.value.map(({ key: weekKey, value: week }) => {
-            console.log("week", week);
             return (
               <tr key={weekKey}>
-                {week.map(({ date, key: dayKey, value: fullDate, }) => {
-                  if (selectedDate.getTime() === fullDate.getTime()) {
+                {week.map(({ date, isCurrentDate, key: dayKey, value: fullDate }) => {
+                  const isSelected = selectedDate?.getTime() === fullDate.getTime();
+
+                  if (isSelected) {
                     console.log('SAME', fullDate);
                   }
-                  return <td key={dayKey} onClick={() => {
-                    console.log('clicked', fullDate);
+
+                  return <DateTableCell $isSelected={isSelected} $isToday={isCurrentDate} key={dayKey} onClick={() => {
                     setSelectedDate(fullDate);
                     closeDatepicker();
-                  }}>{date}</td>;
+                  }}>{date}</DateTableCell>;
                 })}
               </tr>
             );
           })}
-        </tbody>
-      </table>
+        </DateTableBody>
+      </DateTable>
     </Container>
   );
 };
 
-export const DateTimePicker = () => {
+export const DateTimePicker = ({ disabled = false }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [selectedDate, setSelectedDate] = useState<Date>();
 
-console.log('selectedDate', selectedDate);
   const closeDatePicker = () => {
     setIsOpen(false);
   }
 
   return (
     <Dropdown onOpenChange={setIsOpen} open={isOpen}>
-      <Dropdown.Trigger>
-        <DateTimeInputField selectedDate={selectedDate} />
+      <Dropdown.Trigger disabled={disabled}>
+        <DateTimeInputField disabled={disabled} selectedDate={selectedDate} />
       </Dropdown.Trigger>
       <Dropdown.Content>
         <Calendar closeDatepicker={closeDatePicker} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
