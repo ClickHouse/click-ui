5d1c47962c4265faaac593ebe64ee0f6c5de0026
Bucky Schwarz <d.w.schwarz@gmail.com>
2024-12-11 16:23:53 -0500

    Link: export linkStyles to allow styling React Router Links

---
+++ src/components/Link/common.ts
@@ -0,0 +1,36 @@
+import { css } from "styled-components";
+
+export type TextSize = "xs" | "sm" | "md" | "lg";
+export type TextWeight = "normal" | "medium" | "semibold" | "bold";
+
+export type StyledLinkProps = { $size: TextSize; $weight: TextWeight };
+
+export const linkStyles = css<StyledLinkProps>`
+  font: ${({ $size, $weight = "normal", theme }) =>
+    theme.typography.styles.product.text[$weight][$size]};
+  color: ${({ theme }) => theme.click.global.color.text.link.default};
+  margin: 0;
+  text-decoration: none;
+  display: inline-flex;
+  gap: ${({ $size, theme }) =>
+    $size === "xs" || $size === "sm"
+      ? theme.click.link.space.sm.gap
+      : theme.click.link.space.md.gap};
+  margin-right: ${({ $size, theme }) =>
+    $size === "xs" || $size === "sm"
+      ? theme.click.link.space.sm.gap
+      : theme.click.link.space.md.gap};
+  align-items: center;
+
+  &:hover,
+  &:focus {
+    color: ${({ theme }) => theme.click.global.color.text.link.hover};
+    transition: ${({ theme }) => theme.transition.default};
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  &:visited {
+    color: ${({ theme }) => theme.click.global.color.text.link.default};
+  }
+`;
