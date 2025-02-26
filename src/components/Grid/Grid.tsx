79a2c5e50da2ed83b7e4adac1eab32e3e41914df
Nataly Merezhuk <nataly.merezhuk@clickhouse.com>
2025-02-26 14:53:25 -0500

    Removes comments.

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -213,7 +213,6 @@ export const Grid = forwardRef<HTMLDivElement, GridProps>(
       onCopyCallback,
     ]);
 
-    // const rowHeightsRef = useRef(new Map());
     const [rowHeights, setRowHeights] = useState<{ [key: number]: number }>({});
 
     const getRowHeight = useCallback(
@@ -245,33 +244,6 @@ export const Grid = forwardRef<HTMLDivElement, GridProps>(
       },
       [rowAutoHeight]
     );
-
-    // const getRowHeight = useCallback(
-    //   (index: number) => {
-    //     if (rowAutoHeight && rowHeightsRef.current.get(index)) {
-    //       console.log('Rowheightref:', rowHeightsRef)
-    //       return rowHeightsRef.current.get(index) + rowHeight;
-    //     }
-    //     return rowHeight;
-    //   },
-    //   [rowHeight, rowAutoHeight]
-    // );
-
-    // const updateRowHeight = useCallback(
-    //   (rowIndex: number, height: number) => {
-    //     if (!rowAutoHeight) {
-    //       return;
-    //     }
-    //     const prevHeight = rowHeightsRef.current.get(rowIndex) ?? 0;
-    //     if (height > prevHeight) {
-    //       rowHeightsRef.current.set(rowIndex, height);
-    //       if (gridRef.current) {
-    //         gridRef.current.resetAfterRowIndex(rowIndex);
-    //       }
-    //     }
-    //   },
-    //   [rowAutoHeight]
-    // );
 
     const customOnCopy: () => Promise<void> = useMemo(() => {
       const result = async () => {
