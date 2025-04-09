cd4ecb06a5acbf3d516413be17e6c260db634472
Bucky Schwarz <d.w.schwarz@gmail.com>
2025-04-09 11:37:02 -0400

    change how things are rendered to make chrome happy

--- src/components/DatePicker/DateRangePicker.tsx
+++ src/components/DatePicker/DateRangePicker.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { isSameDate, UseCalendarOptions } from "@h6s/calendar";
 import { styled } from "styled-components";
 import Dropdown from "../Dropdown/Dropdown";
-import {
-  CalendarRenderer,
-  DateRangePickerInput,
-  DateTableCell,
-  WeekRenderer,
-} from "./Common";
+import { Body, CalendarRenderer, DateRangePickerInput, DateTableCell } from "./Common";
 
 const DateRangeTableCell = styled(DateTableCell)<{
   $shouldShowRangeIndicator?: boolean;
@@ -22,6 +17,7 @@ const DateRangeTableCell = styled(DateTableCell)<{
 `;
 
 interface CalendarProps {
+  calendarBody: Body;
   closeDatepicker: () => void;
   futureDatesDisabled: boolean;
   setSelectedDate: (selectedDate: Date) => void;
@@ -30,6 +26,7 @@ interface CalendarProps {
 }
 
 const Calendar = ({
+  calendarBody,
   closeDatepicker,
   futureDatesDisabled,
   setSelectedDate,
@@ -49,75 +46,72 @@ const Calendar = ({
     setHoveredDate(undefined);
   };
 
-  const weekRenderer: WeekRenderer = ({
-    date,
-    isCurrentMonth,
-    key: dayKey,
-    value: fullDate,
-  }) => {
-    const isSelected =
-      (startDate && isSameDate(startDate, fullDate)) ||
-      (endDate && isSameDate(endDate, fullDate));
-
-    const today = new Date();
-
-    const isCurrentDate = isSameDate(today, fullDate);
-    const isDisabled = futureDatesDisabled ? fullDate > today : false;
-    const isBetweenStartAndEndDates = Boolean(
-      startDate && endDate && fullDate > startDate && fullDate < endDate
-    );
-
-    const shouldShowRangeIndicator =
-      !endDate &&
-      Boolean(startDate && hoveredDate && fullDate > startDate && fullDate < hoveredDate);
-
-    const handleMouseEnter = () => {
-      setHoveredDate(fullDate);
-    };
-
-    const handleClick = () => {
-      if (isDisabled) {
-        return false;
-      }
-      setSelectedDate(fullDate);
-
-      // User has a date range selected and clicked the selected end date.
-      // This will cause the end date to be unselected, thus do not close the datepicker.
-      if (startDate && endDate && isSameDate(fullDate, endDate)) {
-        return;
-      }
-
-      // Only close the datepicker if the user hasn't clicked the selected start date.
-      if (startDate && !isSameDate(fullDate, startDate)) {
-        closeDatepicker();
-        return;
-      }
-    };
-
+  return calendarBody.value.map(({ key: weekKey, value: week }) => {
     return (
-      <DateRangeTableCell
-        $shouldShowRangeIndicator={shouldShowRangeIndicator || isBetweenStartAndEndDates}
-        $isCurrentMonth={isCurrentMonth}
-        $isDisabled={isDisabled}
-        $isSelected={isSelected}
-        $isToday={isCurrentDate}
-        key={dayKey}
-        onClick={handleClick}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseOut}
-      >
-        {date}
-      </DateRangeTableCell>
-    );
-  };
+      <tr key={weekKey}>
+        {week.map(({ date, isCurrentMonth, key: dayKey, value: fullDate }) => {
+          const isSelected =
+            (startDate && isSameDate(startDate, fullDate)) ||
+            (endDate && isSameDate(endDate, fullDate));
 
-  return (
-    <CalendarRenderer
-      calendarOptions={calendarOptions}
-      onMouseOut={handleMouseOut}
-      weekRenderer={weekRenderer}
-    />
-  );
+          const today = new Date();
+
+          const isCurrentDate = isSameDate(today, fullDate);
+          const isDisabled = futureDatesDisabled ? fullDate > today : false;
+          const isBetweenStartAndEndDates = Boolean(
+            startDate && endDate && fullDate > startDate && fullDate < endDate
+          );
+
+          const shouldShowRangeIndicator =
+            !endDate &&
+            Boolean(
+              startDate && hoveredDate && fullDate > startDate && fullDate < hoveredDate
+            );
+
+          const handleMouseEnter = () => {
+            console.log("mouseEnter");
+            setHoveredDate(fullDate);
+          };
+
+          const handleClick = () => {
+            if (isDisabled) {
+              return false;
+            }
+            setSelectedDate(fullDate);
+
+            // User has a date range selected and clicked the selected end date.
+            // This will cause the end date to be unselected, thus do not close the datepicker.
+            if (startDate && endDate && isSameDate(fullDate, endDate)) {
+              return;
+            }
+
+            // Only close the datepicker if the user hasn't clicked the selected start date.
+            if (startDate && !isSameDate(fullDate, startDate)) {
+              closeDatepicker();
+              return;
+            }
+          };
+          return (
+            <DateRangeTableCell
+              $shouldShowRangeIndicator={
+                shouldShowRangeIndicator || isBetweenStartAndEndDates
+              }
+              $isCurrentMonth={isCurrentMonth}
+              $isDisabled={isDisabled}
+              $isSelected={isSelected}
+              $isToday={isCurrentDate}
+              key={dayKey}
+              onClick={handleClick}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseOut}
+            >
+              {date}
+            </DateRangeTableCell>
+          );
+        })}
+      </tr>
+    );
+  });
 };
 
 export interface DatePickerProps {
@@ -153,49 +147,52 @@ export const DateRangePicker = ({
     }
   }, [endDate]);
 
-  const closeDatePicker = () => {
+  const closeDatePicker = useCallback((): void => {
     setIsOpen(false);
-  };
+  }, []);
 
-  const handleSelectDate = (selectedDate: Date): void => {
-    // Start date and end date are selected, user clicks end date.
-    // Reset the end date.
-    if (selectedEndDate && isSameDate(selectedEndDate, selectedDate)) {
-      setSelectedEndDate(undefined);
-      return;
-    }
+  const handleSelectDate = useCallback(
+    (selectedDate: Date): void => {
+      // Start date and end date are selected, user clicks end date.
+      // Reset the end date.
+      if (selectedEndDate && isSameDate(selectedEndDate, selectedDate)) {
+        setSelectedEndDate(undefined);
+        return;
+      }
 
-    if (selectedStartDate) {
-      if (isSameDate(selectedStartDate, selectedDate)) {
-        // Start date and end date are selected, user clicks start date.
-        // Set the start date to the old end date, reset end date.
-        if (selectedEndDate) {
-          setSelectedStartDate(selectedEndDate);
-          setSelectedEndDate(undefined);
+      if (selectedStartDate) {
+        if (isSameDate(selectedStartDate, selectedDate)) {
+          // Start date and end date are selected, user clicks start date.
+          // Set the start date to the old end date, reset end date.
+          if (selectedEndDate) {
+            setSelectedStartDate(selectedEndDate);
+            setSelectedEndDate(undefined);
+            return;
+          }
+
+          // Start date is selected, user clicks start date.
+          // Reset the start date.
+          setSelectedStartDate(undefined);
           return;
         }
 
-        // Start date is selected, user clicks start date.
-        // Reset the start date.
-        setSelectedStartDate(undefined);
+        // Start date is selected, user clicks an earlier date.
+        // Set the earlier date to the new start date.
+        if (selectedDate < selectedStartDate) {
+          setSelectedStartDate(selectedDate);
+          return;
+        }
+
+        // Otherwise, set the end date to the date the user clicked.
+        setSelectedEndDate(selectedDate);
+        onSelectDateRange(selectedStartDate, selectedDate);
         return;
       }
 
-      // Start date is selected, user clicks an earlier date.
-      // Set the earlier date to the new start date.
-      if (selectedDate < selectedStartDate) {
-        setSelectedStartDate(selectedDate);
-        return;
-      }
-
-      // Otherwise, set the end date to the date the user clicked.
-      setSelectedEndDate(selectedDate);
-      onSelectDateRange(selectedStartDate, selectedDate);
-      return;
-    }
-
-    setSelectedStartDate(selectedDate);
-  };
+      setSelectedStartDate(selectedDate);
+    },
+    [onSelectDateRange, selectedEndDate, selectedStartDate]
+  );
 
   return (
     <Dropdown
@@ -213,13 +210,18 @@ export const DateRangePicker = ({
         />
       </Dropdown.Trigger>
       <Dropdown.Content align="start">
-        <Calendar
-          closeDatepicker={closeDatePicker}
-          futureDatesDisabled={futureDatesDisabled}
-          setSelectedDate={handleSelectDate}
-          startDate={selectedStartDate}
-          endDate={selectedEndDate}
-        />
+        <CalendarRenderer>
+          {body => (
+            <Calendar
+              calendarBody={body}
+              closeDatepicker={closeDatePicker}
+              futureDatesDisabled={futureDatesDisabled}
+              setSelectedDate={handleSelectDate}
+              startDate={selectedStartDate}
+              endDate={selectedEndDate}
+            />
+          )}
+        </CalendarRenderer>
       </Dropdown.Content>
     </Dropdown>
   );
