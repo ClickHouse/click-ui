a11044aade47628613060ec14b40bf92ae79ca4b
Nick Troshkov <nick.troshkov@clickhouse.com>
2025-06-06 15:27:56 +0200

    fix: actually run effects in useUpdateEffect

--- src/hooks/useUpdateEffect.ts
+++ src/hooks/useUpdateEffect.ts
@@ -4,7 +4,7 @@ export const useUpdateEffect: typeof useEffect = (effect, deps) => {
   const isFirstMount = useRef(true);
 
   useEffect(() => {
-    if (isFirstMount) {
+    if (isFirstMount.current) {
       isFirstMount.current = false;
       return;
     }
