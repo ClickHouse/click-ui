496aa1a82c0c1b7a4e357cc2cd7b52a5a584a61f
Nick Troshkov <nick.troshkov@clickhouse.com>
2025-06-13 18:07:16 +0200

    docs(CheckboxMultiSelect): update storybook

--- src/components/Select/CheckboxMultiSelect.stories.tsx
+++ src/components/Select/CheckboxMultiSelect.stories.tsx
@@ -1,142 +1,89 @@
-import { MultiSelectProps } from "..";
+import { useEffect, useState } from "react";
+
+import { Meta, StoryObj } from "@storybook/react";
+
 import { CheckboxMultiSelect } from "./CheckboxMultiSelect";
 import { selectOptions } from "./selectOptions";
-import { useEffect, useState } from "react";
-interface Props extends Omit<MultiSelectProps, "value"> {
-  value: string;
-  childrenType: "children" | "options";
-}
-const CheckboxMultiSelectExample = ({ childrenType, value, ...props }: Props) => {
-  const [selectedValues, setSelectedValues] = useState(
-    value ? value.split(",") : undefined
-  );
-  useEffect(() => {
-    setSelectedValues(value ? value.split(",") : undefined);
-  }, [value]);
 
-  if (childrenType === "options") {
-    return (
-      <CheckboxMultiSelect
-        value={selectedValues}
-        options={selectOptions}
-        onSelect={value => setSelectedValues(value)}
-        selectLabel="Columns;"
-        {...props}
-      />
-    );
-  }
-  return (
-    <CheckboxMultiSelect
-      value={selectedValues}
-      onSelect={value => setSelectedValues(value)}
-      selectLabel="Columns"
-      {...props}
-    >
-      <CheckboxMultiSelect.Group heading="Group label">
-        <CheckboxMultiSelect.Item
-          value="content0"
-          icon="user"
-          iconDir="start"
-        >
-          Content0
-        </CheckboxMultiSelect.Item>
-      </CheckboxMultiSelect.Group>
-      <div>
-        <CheckboxMultiSelect.Item value="content1">
-          Content1 long text content
-        </CheckboxMultiSelect.Item>
-      </div>
-      <CheckboxMultiSelect.Item
-        value="content2"
-        label="Content2"
-      />
-      <CheckboxMultiSelect.Item value="content3">
-        Content3
-        <CheckboxMultiSelect.ItemDescription>
-          Description of Content3
-        </CheckboxMultiSelect.ItemDescription>
-      </CheckboxMultiSelect.Item>
-    </CheckboxMultiSelect>
-  );
-};
-export default {
-  component: CheckboxMultiSelectExample,
+const meta: Meta<typeof CheckboxMultiSelect> = {
+  component: CheckboxMultiSelect,
   title: "Forms/CheckboxMultiSelect",
   tags: ["form-field", "select", "autodocs"],
-  argTypes: {
-    label: { control: "text" },
-    disabled: { control: "boolean" },
-    sortable: { control: "boolean" },
-    error: { control: "text" },
-    value: { control: "text" },
-    defaultValue: { control: "text" },
-    childrenType: { control: "radio", options: ["children", "options"] },
-    name: { control: "text" },
-    required: { control: "boolean" },
-    showSearch: { control: "boolean" },
-    form: { control: "text" },
-    allowCreateOption: { control: "boolean" },
-    showCheck: { control: "boolean" },
-    orientation: { control: "inline-radio", options: ["horizontal", "vertical"] },
-    dir: { control: "inline-radio", options: ["start", "end"] },
-    selectLabel: { control: "text" },
+};
+
+export default meta;
+
+export const OptionsAsChildren: StoryObj<typeof CheckboxMultiSelect> = {
+  args: {
+    label: "Label",
+    value: ["content1"],
+    showSearch: false,
+    selectLabel: "Columns",
+  },
+  render: ({ value, ...props }) => {
+    const [selectedValues, setSelectedValues] = useState(value);
+
+    useEffect(() => {
+      setSelectedValues(value);
+    }, [value]);
+
+    return (
+      <CheckboxMultiSelect
+        {...props}
+        value={selectedValues}
+        onSelect={setSelectedValues}
+      >
+        <CheckboxMultiSelect.Group heading="Group label">
+          <CheckboxMultiSelect.Item
+            value="content0"
+            icon="user"
+            iconDir="start"
+          >
+            Content0
+          </CheckboxMultiSelect.Item>
+        </CheckboxMultiSelect.Group>
+        <div>
+          <CheckboxMultiSelect.Item value="content1">
+            Content1 long text content
+          </CheckboxMultiSelect.Item>
+        </div>
+        <CheckboxMultiSelect.Item
+          value="content2"
+          label="Content2"
+        />
+        <CheckboxMultiSelect.Item value="content3">
+          Content3
+          <CheckboxMultiSelect.ItemDescription>
+            Description of Content3
+          </CheckboxMultiSelect.ItemDescription>
+        </CheckboxMultiSelect.Item>
+      </CheckboxMultiSelect>
+    );
   },
 };
 
-export const Playground = {
+export const OptionsAsProp: StoryObj<typeof CheckboxMultiSelect> = {
   args: {
     label: "Label",
-    value: "content1",
+    value: ["content1"],
     showSearch: false,
-    childrenType: "children",
+    selectLabel: "Columns",
   },
-  parameters: {
-    docs: {
-      source: {
-        transform: (_: string, story: { args: Props; [x: string]: unknown }) => {
-          const { allowCreateOption, childrenType, value, ...props } = story.args;
-          return `<CheckboxMultiSelect\n  value={${JSON.stringify(
-            (value ?? "").split(",")
-          )}}\n${allowCreateOption ? "  allowCreateOption\n" : ""}
-          ${Object.entries(props)
-            .flatMap(([key, value]) =>
-              typeof value === "boolean"
-                ? value
-                  ? `  ${key}`
-                  : []
-                : `  ${key}=${typeof value == "string" ? `"${value}"` : `{${value}}`}`
-            )
-            .join("\n")}
-${
-  childrenType === "options"
-    ? `options={${JSON.stringify(selectOptions, null, 2)}}\n/`
-    : ""
-}>
-${
-  childrenType === "children"
-    ? `
-    <CheckboxMultiSelect.Group heading="Group label">
-      <CheckboxMultiSelect.Item value="content0" icon="user">
-        Content0
-      </CheckboxMultiSelect.Item>
-    </CheckboxMultiSelect.Group>
-    <div>
-      <CheckboxMultiSelect.Item value="content1">Content1 long text content</CheckboxMultiSelect.Item>
-    </div>
-    <CheckboxMultiSelect.Item
-      value="content2"
-      disabled
-    >
-      Content2
-    </CheckboxMultiSelect.Item>
-    <CheckboxMultiSelect.Item value="content3">Content3</CheckboxMultiSelect.Item>
-</CheckboxMultiSelect>
-`
-    : ""
-}`;
-        },
-      },
-    },
+  render: ({ value, ...props }) => {
+    const [selectedValues, setSelectedValues] = useState(value);
+
+    useEffect(() => {
+      setSelectedValues(value);
+    }, [value]);
+
+    return (
+      <CheckboxMultiSelect
+        {...props}
+        options={selectOptions}
+        value={selectedValues}
+        onSelect={value => setSelectedValues(value)}
+      />
+    );
   },
 };
 
