0f2c25ee1780c16d15671583eebb3b7386b40a57
Nikolay Troshkov <nick.troshkov@clickhouse.com>
2025-06-20 14:03:44 +0200

    fix: adjust colors for disabled menu item (#619)

--- src/components/GenericMenu.tsx
+++ src/components/GenericMenu.tsx
@@ -105,8 +105,8 @@ export const GenericMenuItem = styled.div`
       font: ${theme.click.genericMenu.item.typography.label.active};
     }
     &[data-disabled] {
-      background:${theme.click.genericMenu.item.color.background.disabled};
-      color:${theme.click.genericMenu.item.color.text.disabled};
+      // TODO use theme.click.genericMenu.item.color.text.disabled after it's fixed https://github.com/ClickHouse/click-ui/issues/621
+      color:${theme.global.color.text.disabled};
       font: ${theme.click.genericMenu.item.typography.label.disabled};
       pointer-events: none;
     }
