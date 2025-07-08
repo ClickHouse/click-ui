543f16b8b1b4300f3e608d9873d6df572e7c3d54
Nikolay Troshkov <nick.troshkov@clickhouse.com>
2025-07-08 18:39:53 +0200

    fix: expect overflown content in several components (#629)
    * fix: expect overflown content in several components
    
    * docs(CheckboxMultiSelect): add a showcase story

--- src/components/Select/selectOptions.ts
+++ src/components/Select/selectOptions.ts
@@ -6,6 +6,7 @@ export const selectOptions: Array<SelectOptionListItem> = [
     options: [
       {
         icon: "user",
+        iconDir: "start",
         value: "content0",
         label: "Content0",
       },
@@ -29,5 +30,28 @@ export const selectOptions: Array<SelectOptionListItem> = [
   {
     value: "content4",
     label: "Content4",
+  },
+];
+
+export const selectOptionsLong: Array<SelectOptionListItem> = [
+  {
+    value: "1",
+    label:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    value: "2",
+    label:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    value: "3",
+    label:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  },
+  {
+    value: "4",
+    label:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
