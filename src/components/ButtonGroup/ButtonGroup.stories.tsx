c6209538e788707dac56abbe37d9aebb5bfbbc3b
Nick Troshkov <nick.troshkov@clickhouse.com>
2025-07-16 17:33:58 +0200

    docs(ButtonGroup): update storybook

---
+++ src/components/ButtonGroup/ButtonGroup.stories.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import { Meta, StoryObj } from "@storybook/react";
+
+import { ButtonGroup } from "./ButtonGroup";
+
+const meta: Meta<typeof ButtonGroup> = {
+  component: ButtonGroup,
+  title: "Buttons/ButtonGroup",
+  tags: ["button-group", "autodocs"],
+  render: ({ selected, ...props }) => {
+    const [value, setValue] = useState(selected);
+
+    return (
+      <ButtonGroup
+        {...props}
+        selected={value}
+        onClick={setValue}
+      />
+    );
+  },
+};
+
+export default meta;
+
+export const Playground: StoryObj<typeof ButtonGroup> = {
+  args: {
+    options: [
+      { label: "Option 1", value: "option1" },
+      { label: "Option 2", value: "option2" },
+      { label: "Option 3", value: "option3" },
+    ],
+    fillWidth: false,
+    type: "default",
+    selected: "option3",
+  },
+};
