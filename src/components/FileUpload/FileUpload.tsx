4053b93d7ae57076f52d1059614e9cd53eb3ce56
Gareth Jones <github@garethdjones.com>
2025-07-16 11:41:49 -0400

    Updating named imports

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+import { css } from "styled-components";
 import { useState, useRef, useCallback } from "react";
 
 import { truncateFilename } from "@/utils/truncate.ts";
