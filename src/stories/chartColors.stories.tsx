97ed60f07eccf7362c487b0e802b6328c9e8f285
Elizabet Oliveira <elizabet.oliveira@clickhouse.com>
2025-11-17 13:31:21 +0000

    Refactor ChartColorsDemo for improved readability and structure

--- src/stories/chartColors.stories.tsx
+++ src/stories/chartColors.stories.tsx
@@ -1,47 +1,48 @@
 import { Container, Text } from "@/components";
-import styled from "styled-components";
-import { useTheme } from "styled-components";
+import styled, { useTheme } from "styled-components";
 
-const ColorBox = styled(Container)<{
-  $color: string;
-}>`
-  ${({ $color }): string => `
+const ColorBox = styled(Container)<{ $color: string }>`
+  ${({ $color }) => `
     background-color: ${$color};
-  `};
+  `}
   border-radius: 4px;
 `;
 
-const defaultStory = {
+const ChartColorsDemo = () => {
+  const theme = useTheme();
+  return (
+    <Container
+      gap="sm"
+      padding="sm"
+      orientation="horizontal"
+      maxWidth="940px"
+      wrap="wrap"
+    >
+      {Object.entries(theme.global.color.chart.default).map(([name, hex]) => (
+        <Container
+          orientation="vertical"
+          maxWidth="120px"
+          key={name}
+        >
+          <Text>{name}</Text>
+          <ColorBox
+            $color={hex}
+            maxWidth="40px"
+            padding="md"
+          />
+          <Text>{hex}</Text>
+        </Container>
+      ))}
+    </Container>
+  );
+};
+
+export default {
   title: "Colors/Chart Colors",
   tags: ["autodocs", "color", "chart"],
-  render: () => {
-    const theme = useTheme();
-    return (
-      <Container
-        gap="sm"
-        padding="sm"
-        orientation="horizontal"
-      >
-        {Object.entries(theme.global.color.chart.default).map(([name, hex]) => {
-          return (
-            <Container orientation="vertical">
-              <Text>{name}</Text>
-              <ColorBox
-                $color={hex}
-                maxWidth="40px"
-                padding="md"
-              />
-              <Text>{hex}</Text>
-            </Container>
-          );
-        })}
-      </Container>
-    );
-  },
+  render: () => <ChartColorsDemo />,
 };
 
 export const Playground = {
-  ...defaultStory,
+  render: () => <ChartColorsDemo />,
 };
-
-export default defaultStory;
