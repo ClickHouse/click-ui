c434271715210d0d710f35683d89ceedab71b163
Elizabet Oliveira <elizabet.oliveira@clickhouse.com>
2025-11-17 16:23:34 +0000

    Refactor ChartColorComponent and add chart examples

--- src/stories/ChartColorComponent.tsx
+++ src/stories/ChartColorComponent.tsx
@@ -1,36 +1,24 @@
-import { Container, Text } from "@/components";
-import { ReactElement, memo } from "react";
-import clsx from "clsx";
-import styles from "./ChartColorComponent.module.scss";
-import { useCUITheme } from "@/theme/ClickUIProvider";
-import { capitalize } from "@/utils/capitalize";
+import { Container } from "@/components";
+import { ColorSwatchesChart } from "./chart-examples/ColorSwatchesChart";
+import { MultiLineChart } from "./chart-examples/MultiLineChart";
+import { GroupedBarChart } from "./chart-examples/GroupedBarChart";
+import { StackedAreaChart } from "./chart-examples/StackedAreaChart";
+import { PieChartDemo } from "./chart-examples/PieChartDemo";
+import { StackedBarChartDemo } from "./chart-examples/StackedBarChartDemo";
 
-export const ChartColorComponent = memo((): ReactElement => {
-  const { theme } = useCUITheme();
-  return (
-    <Container
-      gap="sm"
-      padding="sm"
-      orientation="horizontal"
-      fillWidth
-    >
-      {Object.entries(theme.global.color.chart.default).map(([name, hex]) => {
-        return (
-          <Container
-            orientation="vertical"
-            fillWidth
-          >
-            <Text>{name}</Text>
-            <Container
-              className={clsx(styles.cuiColorBox, styles[`cui${capitalize(name)}`])}
-              fillWidth
-              maxWidth="40px"
-              padding="md"
-            />
-            <Text>{hex as string}</Text>
-          </Container>
-        );
-      })}
-    </Container>
-  );
-});
+export const ChartColorComponent = () => (
+  <Container
+    gap="lg"
+    padding="md"
+    orientation="vertical"
+    fillWidth
+    maxWidth="900px"
+  >
+    <ColorSwatchesChart />
+    <MultiLineChart />
+    <GroupedBarChart />
+    <StackedAreaChart />
+    <PieChartDemo />
+    <StackedBarChartDemo />
+  </Container>
+);
