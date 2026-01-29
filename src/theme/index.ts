9bb170e12ba1dfc6f6cf6a5e6fd204b5e91bfed8
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-01-29 10:42:51 +0000

    refactor: 💡 remove unnecessary complex types from theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,53 +1,20 @@
 import darkTheme from './tokens/variables.dark';
 import lightTheme from './tokens/variables.light';
 
-// TODO: Can the custom types be simplified
-// bye preferring to use the library provided
-// types for each required case? And where possible
-// can it be inferred instead?
-// Try to make it more readable
-type WidenLiteral<T> = T extends string
-  ? string
-  : T extends number
-    ? number
-    : T extends boolean
-      ? boolean
-      : T;
-
-type GetTypes<T> = {
-  [K in keyof T]: T[K] extends (infer U)[]
-    ? WidenLiteral<U>[]
-    : T[K] extends object
-      ? GetTypes<T[K]>
-      : WidenLiteral<T[K]>;
-};
-
-type Prettify<T> = {
-  [K in keyof T]: T[K] extends object ? Prettify<T[K]> : T[K];
-} & {};
-
 export const THEMES = {
   Dark: 'dark',
   Light: 'light',
 } as const;
 
 export type ThemeName = (typeof THEMES)[keyof typeof THEMES];
+export type Theme = typeof lightTheme;
 
-export type ActiveThemeName = ThemeName;
-
-type GeneralThemeType = typeof lightTheme;
-
-export type Theme = Prettify<
-  Omit<GetTypes<GeneralThemeType>, 'name'> & {
-    name?: ThemeName;
-  }
->;
-
-export const themes: Record<ActiveThemeName, Theme> = {
-  dark: darkTheme as unknown as Theme,
-  light: lightTheme as unknown as Theme,
+export const themes: Record<ThemeName, Theme> = {
+  dark: darkTheme,
+  light: lightTheme,
 };
 
+// TODO: DefaultTheme can now be removed safely
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
 }
