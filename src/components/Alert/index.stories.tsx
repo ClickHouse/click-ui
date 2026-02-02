41054971107156ce1ca75b40b556262acbb07e56
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-02-02 14:01:56 +0000

    fix: 🐛 icon names location

--- src/components/Alert/index.stories.tsx
+++ src/components/Alert/index.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from '@storybook/react-vite';
 import { Alert } from '@/components/Alert';
 import { Container } from '@/components/Container';
 import { Link } from '@/components/Link';
-import { ICON_NAMES } from '@/components/Icon/types';
+import { ICON_NAMES } from '@/components/Icon/IconCommon';
 
 const meta: Meta<typeof Alert> = {
   component: Alert,
