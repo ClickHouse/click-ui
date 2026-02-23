b18ce79b836c8e645024a558eacf5ba852d391df
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-02-23 11:42:10 +0000

    refactor: 💡 remove unused re-export files

--- src/components/Assets/Payments/system/index.ts
+++
@@ -1,5 +0,0 @@
-export { Payment, type PaymentProps } from './Payment';
-export { default as PaymentsLight } from './PaymentsLight';
-export { default as PaymentsDark } from './PaymentsDark';
-export type { PaymentName } from './types';
-export type { SVGAssetProps } from '../../types';
