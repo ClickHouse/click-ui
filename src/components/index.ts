c274dc8a39ab720bb92c2c8cf873ad23d11d255f
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-02-23 15:59:06 +0000

    refactor: 💡 reconsile types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,83 +1,158 @@
-export * from '@/components/types';
+export { Icon } from './Icon';
+export { IconButton } from './IconButton';
+export { Label } from './Label';
+export { GenericLabel } from './GenericLabel';
+export { Dropdown } from './Dropdown';
 
-export { Icon } from '@/components/Icon';
-export { IconButton } from '@/components/IconButton';
-export { Label } from '@/components/Label';
-export { GenericLabel } from '@/components/GenericLabel';
-export { Dropdown } from '@/components/Dropdown';
-
-export { Accordion } from '@/components/Accordion';
+export { Accordion } from './Accordion';
 export {
   Alert,
   DangerAlert,
   InfoAlert,
   WarningAlert,
   SuccessAlert,
-} from '@/components/Alert';
-export { AutoComplete } from '@/components/AutoComplete';
-export { Avatar } from '@/components/Avatar';
-export { Badge } from '@/components/Badge';
-export { BigStat } from '@/components/BigStat';
-export { ButtonGroup } from '@/components/ButtonGroup';
-export { Button } from '@/components/Button';
-export { CardHorizontal } from '@/components/CardHorizontal';
-export { CardPrimary } from '@/components/CardPrimary';
-export { CardPromotion } from '@/components/CardPromotion';
-export { CardSecondary } from '@/components/CardSecondary';
-export { Checkbox } from '@/components/Checkbox';
-export { CodeBlock } from '@/components/CodeBlock';
-export { ConfirmationDialog } from '@/components/ConfirmationDialog';
-export { ContextMenu } from '@/components/ContextMenu';
-export { Container } from '@/components/Container';
-export { DateDetails } from '@/components/DateDetails';
-export { DatePicker } from '@/components/DatePicker';
-export { DateRangePicker } from '@/components/DatePicker/DateRangePicker';
-export { getPredefinedMonthsForDateRangePicker } from '@/components/DatePicker/utils';
-export { Dialog } from '@/components/Dialog';
-export { EllipsisContent } from '@/components/EllipsisContent';
-export { FileUpload } from '@/components/FileUpload';
-export { FileMultiUpload } from '@/components/FileUpload';
-export { Flyout } from '@/components/Flyout';
-export { FormContainer } from '@/components/FormContainer';
-export { GridContainer } from '@/components/GridContainer';
-export { InlineCodeBlock } from '@/components/CodeBlock/InlineCodeBlock';
-export { Flag as Flags } from '@/components/Assets/Flags/system/Flag';
-export { Grid } from '@/components/Grid';
-export { HoverCard } from '@/components/HoverCard';
-export { Link } from '@/components/Link/Link';
-export { linkStyles } from '@/components/Link/common';
-export { Logo } from '@/components/Assets/Logos/system/Logo';
-export { NumberField } from '@/components/Input/NumberField';
-export { PasswordField } from '@/components/Input/PasswordField';
-export { Popover } from '@/components/Popover';
-export { Pagination } from '@/components/Pagination';
-export { Panel } from '@/components/Panel';
-export { ProgressBar } from '@/components/ProgressBar';
-export { RadioGroup } from '@/components/RadioGroup/RadioGroup';
-export { SearchField } from '@/components/Input/SearchField';
-export { Select } from '@/components/Select/SingleSelect';
-export { MultiSelect } from '@/components/Select/MultiSelect';
-export { CheckboxMultiSelect } from '@/components/Select/CheckboxMultiSelect';
-export { Separator } from '@/components/Separator/Separator';
-// TODO: Sidebar elements can be in the ns Sidebar
-// there's no need to keep it separate
-export { SidebarNavigationItem } from '@/components/SidebarNavigationItem';
-export { SidebarCollapsibleItem } from '@/components/SidebarCollapsibleItem';
-export { SidebarNavigationTitle } from '@/components/SidebarNavigationTitle';
-export { SidebarCollapsibleTitle } from '@/components/SidebarCollapsibleTitle';
-export { Spacer } from '@/components/Spacer';
-export { SplitButton } from '@/components/SplitButton';
-export { Switch } from '@/components/Switch';
-export { Tabs, FullWidthTabs } from '@/components/Tabs';
-export { FileTabs, FileTabElement } from '@/components/FileTabs';
-export { Table } from '@/components/Table/Table';
-export { Text, Title } from '@/components/Typography';
-export { TextAreaField } from '@/components/Input/TextArea';
-export { TextField } from '@/components/Input/TextField';
-export { Tooltip } from '@/components/Tooltip/Tooltip';
-export { useToast } from '@/components/Toast/useToast';
-export { createToast } from '@/components/Toast/toastEmitter';
-export { User as ProfileIcon } from '@/components/Assets/Icons/User';
-export { VerticalStepper } from '@/components/VerticalStepper';
-export { MultiAccordion } from '@/components/MultiAccordion';
-export { ToastProvider, Toast } from '@/components/Toast';
+} from './Alert';
+export { AutoComplete } from './AutoComplete';
+export { Avatar } from './Avatar';
+export { Badge } from './Badge';
+export { BigStat } from './BigStat';
+export { ButtonGroup } from './ButtonGroup';
+export { Button } from './Button';
+export { CardHorizontal } from './CardHorizontal';
+export { CardPrimary } from './CardPrimary';
+export { CardPromotion } from './CardPromotion';
+export { CardSecondary } from './CardSecondary';
+export { Checkbox } from './Checkbox';
+export { CodeBlock } from './CodeBlock';
+export { ConfirmationDialog } from './ConfirmationDialog';
+export { ContextMenu } from './ContextMenu';
+export { Container } from './Container';
+export { DateDetails } from './DateDetails';
+export { DatePicker } from './DatePicker';
+export { DateRangePicker } from './DatePicker/DateRangePicker';
+export { getPredefinedMonthsForDateRangePicker } from './DatePicker/utils';
+export { Dialog } from './Dialog';
+export { EllipsisContent } from './EllipsisContent';
+export { FileUpload } from './FileUpload';
+export { FileMultiUpload } from './FileUpload';
+export { Flyout } from './Flyout';
+export { FormContainer } from './FormContainer';
+export { GridContainer } from './GridContainer';
+export { InlineCodeBlock } from './CodeBlock/InlineCodeBlock';
+export { Flag as Flags } from './Assets/Flags/system/Flag';
+export { Grid } from './Grid';
+export { HoverCard } from './HoverCard';
+export { Link } from './Link/Link';
+export { linkStyles } from './Link/common';
+export { Logo } from './Assets/Logos/system/Logo';
+export { NumberField } from './Input/NumberField';
+export { PasswordField } from './Input/PasswordField';
+export { Popover } from './Popover';
+export { Pagination } from './Pagination';
+export { Panel } from './Panel';
+export { ProgressBar } from './ProgressBar';
+export { RadioGroup } from './RadioGroup/RadioGroup';
+export { SearchField } from './Input/SearchField';
+export { Select } from './Select/SingleSelect';
+export { MultiSelect } from './Select/MultiSelect';
+export { CheckboxMultiSelect } from './Select/CheckboxMultiSelect';
+export { Separator } from './Separator/Separator';
+export { SidebarNavigationItem } from './SidebarNavigationItem';
+export { SidebarCollapsibleItem } from './SidebarCollapsibleItem';
+export { SidebarNavigationTitle } from './SidebarNavigationTitle';
+export { SidebarCollapsibleTitle } from './SidebarCollapsibleTitle';
+export { Spacer } from './Spacer';
+export { SplitButton } from './SplitButton';
+export { Switch } from './Switch';
+export { Tabs, FullWidthTabs } from './Tabs';
+export { FileTabs, FileTabElement } from './FileTabs';
+export { Table } from './Table/Table';
+export { Text, Title } from './Typography';
+export { TextAreaField } from './Input/TextArea';
+export { TextField } from './Input/TextField';
+export { Tooltip } from './Tooltip/Tooltip';
+export { useToast } from './Toast/useToast';
+export { createToast } from './Toast/toastEmitter';
+export { User as ProfileIcon } from './Assets/Icons/User';
+export { VerticalStepper } from './VerticalStepper';
+export { MultiAccordion } from './MultiAccordion';
+export { ToastProvider, Toast } from './Toast';
+
+export type { AlertProps } from './Alert';
+export type {
+  AutoCompleteProps,
+  AutoCompleteOptionListItem,
+  SelectOptionItem,
+  SelectItemProps,
+  SelectGroupProps,
+} from './AutoComplete';
+export type { AvatarProps } from './Avatar';
+export type { BadgeProps } from './Badge';
+export type { BigStatProps } from './BigStat';
+export type { ButtonProps, ButtonType } from './Button';
+export type { ButtonGroupProps } from './ButtonGroup';
+export type { CardHorizontalProps } from './CardHorizontal';
+export type { CardPrimaryProps } from './CardPrimary';
+export type { CardPromotionProps } from './CardPromotion';
+export type { CardSecondaryProps, BadgeState } from './CardSecondary';
+export type { CheckboxProps, CheckboxVariants } from './Checkbox';
+export type { States, HorizontalDirection, Orientation, CursorOptions } from './Common';
+export type { ConfirmationDialogProps } from './ConfirmationDialog';
+export type { ContainerProps } from './Container';
+export type { ContextMenuItemProps } from './ContextMenu';
+export type { DateRange } from './DatePicker/utils';
+export type { DialogContentProps } from './Dialog';
+export type { FileTabStatusType } from './FileTabs';
+export type { FlyoutProps, FlyoutFooterProps, FlyoutHeaderProps } from './Flyout';
+export type { FormContainerProps } from './FormContainer';
+export type { GenericLabelProps } from './GenericLabel';
+export type {
+  GridProps,
+  CellProps,
+  SelectedRegion,
+  SelectionFocus,
+  SelectionPos,
+  SelectionAction,
+  GridContextMenuItemProps,
+  Rectangle,
+} from './Grid/types';
+export type { GridContainerProps } from './GridContainer';
+export type { IconButtonProps } from './IconButton';
+export type { ImageName as IconName } from './Icon/types';
+export type { LabelProps } from './Label';
+export type { StyledLinkProps } from './Link/common';
+export type { MultiAccordionProps } from './MultiAccordion';
+export type { NumberFieldProps } from './Input/NumberField';
+export type { PaginationProps } from './Pagination';
+export type { PanelProps } from './Panel';
+export type { PasswordFieldProps } from './Input/PasswordField';
+export type { ProgressBarProps } from './ProgressBar';
+export type { RadioGroupProps, RadioGroupItemProps } from './RadioGroup';
+export type { SearchFieldProps } from './Input/SearchField';
+export type { SelectProps } from './Select/SingleSelect';
+export type { SelectOptionListItem, SelectGroupOptionItem } from './Select/common/types';
+export type { MultiSelectProps } from './Select/MultiSelect';
+export type { CheckboxMultiSelectProps } from './Select/CheckboxMultiSelect';
+export type { SidebarCollapsibleItemProps } from './SidebarCollapsibleItem';
+export type { SidebarCollapsibleTitleProps } from './SidebarCollapsibleTitle';
+export type { SidebarNavigationItemProps } from './SidebarNavigationItem';
+export type { SidebarNavigationTitleProps } from './SidebarNavigationTitle';
+export type { SpacerProps } from './Spacer';
+export type { Menu, SplitButtonProps } from './SplitButton';
+export type { TabsProps } from './Tabs';
+export type {
+  TableHeaderType,
+  TableRowType,
+  TableProps,
+  TableColumnConfigProps,
+} from './Table';
+export type { TextAreaFieldProps } from './Input/TextArea';
+export type { ToastProps } from './Toast';
+export type { TooltipProps } from './Tooltip';
+export type { TextProps } from './Typography/Text/Text';
+export type { TitleProps } from './Typography/Title/Title';
+export type { VerticalStepperProps, VerticalStepProps } from './VerticalStepper';
+export type { ContextMenuProps } from '@radix-ui/react-context-menu';
+export type { DialogProps, DialogTriggerProps } from '@radix-ui/react-dialog';
+export type { HoverCardProps } from '@radix-ui/react-hover-card';
+export type { PopoverProps } from '@radix-ui/react-popover';
