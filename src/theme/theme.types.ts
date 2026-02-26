0838edd1583059fa3fc37618920910cd52097c13
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-02-26 15:27:07 +0000

    refactor: 💡 move THEMES data structure from types

--- src/theme/theme.types.ts
+++ src/theme/theme.types.ts
@@ -1,13 +1,8 @@
 import lightTheme from './tokens/variables.light';
 
-export const THEMES = {
-  Dark: 'dark',
-  Light: 'light',
-} as const;
-
-export type ThemeName = (typeof THEMES)[keyof typeof THEMES];
-
 export type Theme = typeof lightTheme;
+
+export type ThemeName = 'dark' | 'light';
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
