bba76b6b0e7e504230085cb2fd04817d99f1e047
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-03-05 11:32:25 +0000

    fix: 🐛 resolve name for icons

--- src/components/Assets/Icons/system/retroactiveNames.ts
+++ src/components/Assets/Icons/system/retroactiveNames.ts
@@ -1,6 +1,6 @@
 import { IconName } from './types';
 import { resolveAssetName } from '@/components/Assets/config';
 
-export const resolveFlagName = (name: string): IconName => {
+export const resolveIconName = (name: string): IconName => {
   return resolveAssetName(name) as IconName;
 };
