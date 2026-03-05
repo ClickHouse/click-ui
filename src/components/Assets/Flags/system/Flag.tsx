76a77e8e6fcde52669597699eecf755aef11187e
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-03-05 11:46:34 +0000

    refactor: 💡 adapt resolver in flag system

--- src/components/Assets/Flags/system/Flag.tsx
+++ src/components/Assets/Flags/system/Flag.tsx
@@ -1,18 +1,34 @@
 import { SVGAttributes } from 'react';
 import { useTheme } from 'styled-components';
-import { getFallbackThemeName } from '@/theme/theme.utils';
-import { SvgImageElement } from '@/components/Icon/SvgImageElement';
-import { FlagProps } from './types';
-import { resolveFlagName } from './retroactiveNames';
-import FlagsDark from './FlagsDark';
+import { IconSize } from '@/components/Icon/types';
+import { FlagName } from './types';
+export type { FlagName, SVGAssetProps } from './types';
 import FlagsLight from './FlagsLight';
+import FlagsDark from './FlagsDark';
+import { SvgImageElement } from '@/components/commonElement';
+import { type ThemeName, THEMES } from '@/theme';
+import {
+  createAssetResolver,
+  type AssetAlias,
+  type AssetDeprecatedName,
+} from '@/components/Assets/config';
+
+const resolveFlagName = createAssetResolver<FlagName>();
+
+export { resolveFlagName };
+
+export interface FlagProps extends SVGAttributes<SVGElement> {
+  name: FlagName | AssetAlias | AssetDeprecatedName;
+  theme?: ThemeName;
+  size?: IconSize;
+}
 
 const Flag = ({ name, theme, size, ...props }: FlagProps) => {
   const { name: themeName } = useTheme();
   const resolvedName = resolveFlagName(name);
-  const resolvedTheme = getFallbackThemeName(theme ?? themeName);
+  const resolvedTheme: ThemeName = theme ?? (themeName as ThemeName) ?? THEMES.Light;
   const Component =
-    resolvedTheme === 'light' ? FlagsLight[resolvedName] : FlagsDark[resolvedName];
+    resolvedTheme === THEMES.Dark ? FlagsLight[resolvedName] : FlagsDark[resolvedName];
 
   if (!Component) {
     return null;
