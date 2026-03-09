8454c0b485629c5e2202fa4ff799553c2663ca45
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-03-09 17:17:59 +0000

    fix: 🐛 date picker props

--- src/components/DatePicker/index.ts
+++ src/components/DatePicker/index.ts
@@ -2,6 +2,6 @@ export { DatePicker } from './DatePicker';
 export { DateRangePicker } from './DateRangePicker';
 export { DateTimeRangePicker } from './DateTimeRangePicker';
 
-export type { DatePickerProps } from './DatePicker.types';
+export type { DatePickerProps } from './DatePicker';
 export type { DateRangePickerProps } from './DateRangePicker';
 export type { DateTimeRangePickerProps } from './DateTimeRangePicker';
