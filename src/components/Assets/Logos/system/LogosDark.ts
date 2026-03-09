f702772e91e7ea0b509fb0e2a36d70eeca9745d6
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-03-09 18:08:04 +0000

    refactor: 💡 normalise names

--- src/components/Assets/Logos/system/LogosDark.ts
+++ src/components/Assets/Logos/system/LogosDark.ts
@@ -6,7 +6,7 @@
  */
 
 import Airbyte from '../Airbyte';
-import AlloyDB from '../AlloyDB';
+import Alloydb from '../Alloydb';
 import AWS from '../AWS';
 import AWS_ATHENA from '../AWS_ATHENA';
 import AWS_AURORA from '../AWS_AURORA';
@@ -19,20 +19,20 @@ import AWS_S3 from '../AWS_S3';
 import Azure from '../Azure';
 import Azure_Blob_Storage from '../Azure_Blob_Storage';
 import Azure_Event_Hub from '../Azure_Event_Hub';
-import BigQuery from '../BigQuery';
-import C_SHARP from '../C_SHARP';
+import Bigquery from '../Bigquery';
+import Csharp from '../Csharp';
 import Clickhouse from '../Clickhouse';
 import Cloudflare from '../Cloudflare';
 import Confluent from '../Confluent';
 import Crunchy_Bridge from '../Crunchy_Bridge';
-import dBeaver from '../dBeaver';
-import DataGrip from '../DataGrip';
+import Dbeaver from '../Dbeaver';
+import Datagrip from '../Datagrip';
 import Databricks from '../Databricks';
 import Dbt from '../Dbt';
 import Decodeable from '../Decodeable';
-import DeepNote from '../DeepNote';
-import DeltaLake from '../DeltaLake';
-import DigitalOcean from '../DigitalOcean';
+import Deepnote from '../Deepnote';
+import Deltalake from '../Deltalake';
+import Digitalocean from '../Digitalocean';
 import FeatureDatabase from '../FeatureDatabase';
 import FeatureHexagon from '../FeatureHexagon';
 import Fivetran from '../Fivetran';
@@ -47,16 +47,16 @@ import Hudi from '../Hudi';
 import Iceberg from '../Iceberg';
 import Jdbc from '../Jdbc';
 import Kafka from '../Kafka';
-import Kubenetes from '../Kubenetes';
-import MariaDB from '../MariaDB';
+import Kubernetes from '../Kubernetes';
+import Mariadb from '../Mariadb';
 import Metabase from '../Metabase';
 import Microsoft from '../Microsoft';
 import Mongodb from '../Mongodb';
 import Mysql from '../Mysql';
-import NeonDB from '../NeonDB';
+import Neondb from '../Neondb';
 import Nessie from '../Nessie';
 import Nodejs from '../Nodejs';
-import OneLake from '../OneLake';
+import Onelake from '../Onelake';
 import OVH from '../OVH';
 import Planetscale from '../Planetscale';
 import Postgres from '../Postgres';
@@ -68,17 +68,17 @@ import Snowflake from '../Snowflake';
 import Supabase from '../Supabase';
 import Superset from '../Superset';
 import Tableau from '../Tableau';
-import TigerData from '../TigerData';
+import Tigerdata from '../Tigerdata';
 import Upstash from '../Upstash';
 import Vector from '../Vector';
-import WarpStream from '../WarpStream';
+import Warpstream from '../Warpstream';
 import { LogoName } from './types';
 import type { SVGAssetProps } from '@/types';
 import type { ComponentType } from 'react';
 
 const LogosDark: Record<LogoName, ComponentType<SVGAssetProps>> = {
   airbyte: Airbyte,
-  'alloy-db': AlloyDB,
+  alloydb: Alloydb,
   aws: AWS,
   'aws-athena': AWS_ATHENA,
   'aws-aurora': AWS_AURORA,
@@ -91,20 +91,20 @@ const LogosDark: Record<LogoName, ComponentType<SVGAssetProps>> = {
   azure: Azure,
   'azure-blob-storage': Azure_Blob_Storage,
   'azure-event-hub': Azure_Event_Hub,
-  'big-query': BigQuery,
-  'c-sharp': C_SHARP,
+  bigquery: Bigquery,
+  csharp: Csharp,
   clickhouse: Clickhouse,
   cloudflare: Cloudflare,
   confluent: Confluent,
   'crunchy-bridge': Crunchy_Bridge,
-  'd-beaver': dBeaver,
-  'data-grip': DataGrip,
+  dbeaver: Dbeaver,
+  datagrip: Datagrip,
   databricks: Databricks,
   dbt: Dbt,
   decodeable: Decodeable,
-  'deep-note': DeepNote,
-  'delta-lake': DeltaLake,
-  'digital-ocean': DigitalOcean,
+  deepnote: Deepnote,
+  deltalake: Deltalake,
+  digitalocean: Digitalocean,
   'feature-database': FeatureDatabase,
   'feature-hexagon': FeatureHexagon,
   fivetran: Fivetran,
@@ -119,16 +119,16 @@ const LogosDark: Record<LogoName, ComponentType<SVGAssetProps>> = {
   iceberg: Iceberg,
   jdbc: Jdbc,
   kafka: Kafka,
-  kubenetes: Kubenetes,
-  'maria-db': MariaDB,
+  kubernetes: Kubernetes,
+  mariadb: Mariadb,
   metabase: Metabase,
   microsoft: Microsoft,
   mongodb: Mongodb,
   mysql: Mysql,
-  'neon-db': NeonDB,
+  neondb: Neondb,
   nessie: Nessie,
   nodejs: Nodejs,
-  'one-lake': OneLake,
+  onelake: Onelake,
   ovh: OVH,
   planetscale: Planetscale,
   postgres: Postgres,
@@ -140,10 +140,10 @@ const LogosDark: Record<LogoName, ComponentType<SVGAssetProps>> = {
   supabase: Supabase,
   superset: Superset,
   tableau: Tableau,
-  'tiger-data': TigerData,
+  tigerdata: Tigerdata,
   upstash: Upstash,
   vector: Vector,
-  'warp-stream': WarpStream,
+  warpstream: Warpstream,
 };
 
 export default LogosDark;
