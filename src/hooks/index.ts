5b631b2ad2290981872a931fa2cd51b31f740a9c
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-03-11 15:55:55 +0000

    refactor: 💡 hook-level public exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
-export { useUpdateEffect } from './useUpdateEffect';
-export { useInitialTheme } from './useInitialTheme';
-export { useInputModality } from './useInputModality';
 export { useCUITheme } from './useCUITheme';
-export type { CUIThemeType } from './useCUITheme';
 export { useToast } from './useToast';
+export { useInitialTheme } from './useInitialTheme';
+
+export type { CUIThemeType } from './useCUITheme';
