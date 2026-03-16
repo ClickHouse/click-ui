e29e9f17050bbc7d7bc16c9d99100e0086b71907
Helder Oliveira <helder.oliveira@clickhouse.com>
2026-03-16 18:27:08 +0000

    refactor: 💡 generate tokens, e.g. theme config shared file as json, remove generate quotes,e tc

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,1 +1,7 @@
-export { CUI_THEME_STORAGE_KEY } from './constants.js';
+// WARN: The storage key is shared with .scripts/js/generate-token
+// which is a nodejs script. At the moment opted for json instead
+// of adding support for `js` files, but json means importing the
+// whole file (the file shouldn't grow, so should be alright?)
+import config from '@/theme/theme.config.json' with { type: 'json' };
+
+export const CUI_THEME_STORAGE_KEY = config.storageKey;
