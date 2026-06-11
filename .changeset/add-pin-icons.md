88e265115b34ee4d5a0b344c9522e18607b75316
Kunal Piush Kumar <hello@kunalpanchal.in>
2026-06-11 13:00:30 +0200

    chore(new-icons): Add pin & unpin icons (#1066)
    * Add pin icons
    
    * Adjust pin icon scale
    
    * Tune pin icon scale
    
    * Add changeset for pin icons
    
    * Update pin icons

---
+++ .changeset/add-pin-icons.md
@@ -0,0 +1,5 @@
+---
+'@clickhouse/click-ui': minor
+---
+
+Add `pin` and `unpin` icons to the icon asset set.
---
+++ src/components/Assets/Icons/Unpin.tsx
@@ -0,0 +1,57 @@
+import type { SVGAssetProps } from '@/types';
+
+const Unpin = (props: SVGAssetProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      d="M4.99709 20.003L9.32338 15.6768"
+      stroke="#323232"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13.7068 7.12866C13.9626 6.87754 14.09 6.52345 14.0527 6.16688L13.8798 4.65155C13.8515 4.40278 13.9839 4.1634 14.2096 4.05503C14.4353 3.94667 14.7049 3.99307 14.8814 4.17066L20.8302 10.1195C21.0078 10.296 21.0542 10.5656 20.9459 10.7913C20.8375 11.017 20.5981 11.1493 20.3493 11.1211L18.834 10.9481C18.4774 10.9108 18.1233 11.0382 17.8722 11.2941"
+      stroke="#323232"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15.4955 13.6703C15.2736 13.8903 15.149 14.1899 15.1495 14.5024V18.6643C15.1487 19.1402 14.8617 19.5689 14.422 19.7509C13.9823 19.9329 13.4763 19.8325 13.1394 19.4964L5.50917 11.8592C5.17305 11.5223 5.07264 11.0163 5.25465 10.5766C5.43666 10.1369 5.86534 9.84989 6.34121 9.84913H10.5032C10.8135 9.84866 11.111 9.72481 11.33 9.50488"
+      stroke="#323232"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13.4188 7.41667L13.7068 7.12866"
+      stroke="#323232"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M17.8722 11.2937L17.5842 11.5817"
+      stroke="#323232"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M20.0033 18.0025L6.99792 4.99707"
+      stroke="#323232"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Unpin;
